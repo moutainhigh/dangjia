a09057e52344263849c3b49256ba5795e9c4dcdd
qiyuxiang <qiyuxiangjava@qq.com>
2019-01-08 10:26:33 +0800

    123

--- dangjia-subgroup/dangjia-upload/dangjia-service-upload/src/main/java/com/dangjia/acg/config/ConstantProperties.java
+++ dangjia-subgroup/dangjia-upload/dangjia-service-upload/src/main/java/com/dangjia/acg/config/ConstantProperties.java
@@ -1,16 +1,40 @@
 package com.dangjia.acg.config;
- 
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by qiyuxiang
- * Date 2019/1/5
+ * Date 2018/1/16
  * Description:配置文件配置项
  */
-public class ConstantProperties {
+@Component
+public class ConstantProperties implements InitializingBean {
+
+    @Value("${huaWei.obs.endPoint}")
+    private String huawei_file_endpoint;
+
+    @Value("${huaWei.obs.keyId}")
+    private String huawei_file_keyid;
+
+    @Value("${huaWei.obs.keySecret}")
+    private String huawei_file_keysecret;
+
+    @Value("${huaWei.obs.bucketName}")
+    private String huawei_file_bucketname;
 
 
-    public static String HUAWEI_END_POINT="https://obs.cn-south-1.myhuaweicloud.com";
-    public static String HUAWEI_ACCESS_KEY_ID="KU8IPBQCNKNDOPMVDRDM";
-    public static String HUAWEI_ACCESS_KEY_SECRET="d2T2CFKnjY9FqTkxqqD0wDOTV8h5lQFvf7OhxTIV";
-    public static String HUAWEI_BUCKET_NAME="dangjia";
- 
+    public static String HUAWEI_END_POINT;
+    public static String HUAWEI_ACCESS_KEY_ID;
+    public static String HUAWEI_ACCESS_KEY_SECRET;
+    public static String HUAWEI_BUCKET_NAME;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        HUAWEI_END_POINT = huawei_file_endpoint;
+        HUAWEI_ACCESS_KEY_ID = huawei_file_keyid;
+        HUAWEI_ACCESS_KEY_SECRET = huawei_file_keysecret;
+        HUAWEI_BUCKET_NAME = huawei_file_bucketname;
+    }
 }
