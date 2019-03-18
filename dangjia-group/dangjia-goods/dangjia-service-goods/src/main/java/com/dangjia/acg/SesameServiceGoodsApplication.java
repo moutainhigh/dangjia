c2df4888cf9733379172a032be4e419ea14b2534
qiyuxiang <qiyuxiangjava@qq.com>
2019-03-18 13:53:15 +0800

    12

--- dangjia-group/dangjia-goods/dangjia-service-goods/src/main/java/com/dangjia/acg/SesameServiceGoodsApplication.java
+++ dangjia-group/dangjia-goods/dangjia-service-goods/src/main/java/com/dangjia/acg/SesameServiceGoodsApplication.java
@@ -41,12 +41,7 @@ public class SesameServiceGoodsApplication implements CommandLineRunner {
     corsConfiguration.addAllowedMethod("*");
     return corsConfiguration;
   }
-  @Bean
-  MultipartConfigElement multipartConfigElement() {
-    MultipartConfigFactory factory = new MultipartConfigFactory();
-    factory.setLocation("/data/dangjia/temporary/");
-    return factory.createMultipartConfig();
-  }
+
   @Override
   public void run(String... args) throws Exception {
     String fozuStr = "CiAgICAgICAgICAgICAgICAgICBfb29Pb29fCiAgICAgICAgICAgICAgICAgIG84ODg4ODg4bwogICAgICAgICAgICAgICAgICA4OCIgLiAiODgKICAgICAgICAgICAgICAgICAgKHwgLV8tIHwpCiAgICAgICAgICAgICAgICAgIE9cICA9ICAvTwogICAgICAgICAgICAgICBfX19fL2AtLS0nXF9fX18KICAgICAgICAgICAgIC4nICBcXHwgICAgIHwvLyAgYC4KICAgICAgICAgICAgLyAgXFx8fHwgIDogIHx8fC8vICBcCiAgICAgICAgICAgLyAgX3x8fHx8IC06LSB8fHx8fC0gIFwKICAgICAgICAgICB8ICAgfCBcXFwgIC0gIC8vLyB8ICAgfAogICAgICAgICAgIHwgXF98ICAnJ1wtLS0vJycgIHwgICB8CiAgICAgICAgICAgXCAgLi1cX18gIGAtYCAgX19fLy0uIC8KICAgICAgICAgX19fYC4gLicgIC8tLS4tLVwgIGAuIC4gX18KICAgICAgLiIiICc8ICBgLl9fX1xfPHw+Xy9fX18uJyAgPiciIi4KICAgICB8IHwgOiAgYC0gXGAuO2BcIF8gL2A7LmAvIC0gYCA6IHwgfAogICAgIFwgIFwgYC0uICAgXF8gX19cIC9fXyBfLyAgIC4tYCAvICAvCj09PT09PWAtLl9fX19gLS5fX19cX19fX18vX19fLi1gX19fXy4tJz09PT09PQogICAgICAgICAgICAgICAgICAgYD0tLS09JwpeXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl5eXl4KICAgICAg5L2b56WW5L+d5L2RLOWkp+WQieWkp+WIqe+8jOebruagh+WQg+ifue+8jOawuOaXoEJVRw==";
