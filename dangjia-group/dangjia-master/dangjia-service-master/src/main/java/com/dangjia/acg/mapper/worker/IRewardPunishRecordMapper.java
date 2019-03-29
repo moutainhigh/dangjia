14625babce8e0628ea5bee77b160bcb6baf36f3d
maoyinyu <894678458@qq.com>
2019-03-29 18:13:46 +0800

    添加申述模块

--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/mapper/worker/IRewardPunishRecordMapper.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/mapper/worker/IRewardPunishRecordMapper.java
@@ -2,12 +2,15 @@ package com.dangjia.acg.mapper.worker;
 
 import com.dangjia.acg.dto.worker.RewardPunishRecordDTO;
 import com.dangjia.acg.modle.worker.RewardPunishRecord;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
 import java.util.List;
 
 @Repository
-public interface IRewardPunishRecordMapper extends Mapper<RewardPunishRecord>{
+public interface IRewardPunishRecordMapper extends Mapper<RewardPunishRecord> {
     List<RewardPunishRecordDTO> queryRewardPunishRecord(RewardPunishRecordDTO rewardPunishRecordDTO);
+
+    RewardPunishRecordDTO getRewardPunishRecord(@Param("rewardPunishRecordId") String rewardPunishRecordId);
 }
