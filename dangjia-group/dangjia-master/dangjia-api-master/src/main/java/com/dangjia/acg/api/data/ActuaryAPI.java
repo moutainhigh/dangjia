175bc953944111ff7e80882f608659e67932e88c
qiyuxiang <qiyuxiangjava@qq.com>
2019-04-07 15:15:36 +0800

    优化

--- dangjia-group/dangjia-master/dangjia-api-master/src/main/java/com/dangjia/acg/api/data/ActuaryAPI.java
+++ dangjia-group/dangjia-master/dangjia-api-master/src/main/java/com/dangjia/acg/api/data/ActuaryAPI.java
@@ -1,11 +1,14 @@
 package com.dangjia.acg.api.data;
 
+import com.dangjia.acg.common.model.PageDTO;
 import com.dangjia.acg.common.response.ServerResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * author: Ronalcheng
@@ -18,23 +21,44 @@ public interface ActuaryAPI {
 
     @PostMapping("/data/actuary/getActuaryWaitPay")
     @ApiOperation(value = "返回待业主支付精算列表", notes = "返回待业主支付精算列表")
-    ServerResponse getActuaryWaitPay();
+    ServerResponse getActuaryWaitPay(
+            @RequestParam("request") HttpServletRequest request,
+            @RequestParam("pageDTO") PageDTO pageDTO,
+            @RequestParam("name") String name
+
+    );
 
     @PostMapping("/data/actuary/getActuaryCommit")
     @ApiOperation(value = "返回待提交精算列表", notes = "返回待提交精算列表")
-    ServerResponse getActuaryCommit();
+    ServerResponse getActuaryCommit(
+            @RequestParam("request") HttpServletRequest request,
+            @RequestParam("pageDTO") PageDTO pageDTO,
+            @RequestParam("name") String name
+    );
 
     @PostMapping("/data/actuary/getActuaryConfirm")
     @ApiOperation(value = "返回待业主确认精算列表", notes = "返回待业主确认精算列表")
-    ServerResponse getActuaryConfirm();
+    ServerResponse getActuaryConfirm(
+            @RequestParam("request") HttpServletRequest request,
+            @RequestParam("pageDTO") PageDTO pageDTO,
+            @RequestParam("name") String name
+    );
 
     @PostMapping("/data/actuary/getActuaryComplete")
     @ApiOperation(value = "返回已完成精算列表", notes = "返回已完成精算列表")
-    ServerResponse getActuaryComplete();
+    ServerResponse getActuaryComplete(
+            @RequestParam("request") HttpServletRequest request,
+            @RequestParam("pageDTO") PageDTO pageDTO,
+            @RequestParam("name") String name
+    );
 
     @PostMapping("/data/actuary/getStatistics")
     @ApiOperation(value = "返回统计列表", notes = "返回统计列表")
-    ServerResponse getStatistics();
+    ServerResponse getStatistics(
+            @RequestParam("request") HttpServletRequest request,
+            @RequestParam("pageDTO") PageDTO pageDTO,
+            @RequestParam("name") String name
+    );
 
     @PostMapping("/data/actuary/getStatisticsByDate")
     @ApiOperation(value = "返回按日期统计列表", notes = "返回按日期统计列表")
