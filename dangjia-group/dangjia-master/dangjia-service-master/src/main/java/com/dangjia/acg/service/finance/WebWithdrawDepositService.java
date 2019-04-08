6375f1a2221b856cbc8131fe2cd596e8a74fbb01
qiyuxiang <qiyuxiangjava@qq.com>
2019-04-08 19:56:52 +0800

    123

--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/finance/WebWithdrawDepositService.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/finance/WebWithdrawDepositService.java
@@ -1,9 +1,12 @@
 package com.dangjia.acg.service.finance;
 
 import com.dangjia.acg.common.constants.DjConstants;
+import com.dangjia.acg.common.constants.SysConfig;
 import com.dangjia.acg.common.model.PageDTO;
 import com.dangjia.acg.common.response.ServerResponse;
+import com.dangjia.acg.common.util.CommonUtil;
 import com.dangjia.acg.common.util.DateUtil;
+import com.dangjia.acg.dao.ConfigUtil;
 import com.dangjia.acg.dto.finance.WebWithdrawDTO;
 import com.dangjia.acg.mapper.member.IMemberMapper;
 import com.dangjia.acg.mapper.worker.IWithdrawDepositMapper;
@@ -15,14 +18,10 @@ import com.dangjia.acg.service.config.ConfigMessageService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import tk.mybatis.mapper.entity.Example;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -41,68 +40,32 @@ public class WebWithdrawDepositService {
     private IWithdrawDepositMapper iWithdrawDepositMapper;
     @Autowired
     private ConfigMessageService configMessageService;
+    @Autowired
+    private ConfigUtil configUtil;
 
-
-    private static Logger LOG = LoggerFactory.getLogger(WebWithdrawDepositService.class);
 
     /**
      * 查询所有提现申请
+     *
+     * @param pageDTO   分页码
+     * @param searchKey 模糊查询：名称，手机号
+     * @param state     状态
+     * @param beginDate 开始时间
+     * @param endDate   结束时间
+     * @return
      */
-    public ServerResponse getAllWithdraw(PageDTO pageDTO, Integer state, String beginDate, String endDate) {
+    public ServerResponse getAllWithdraw(PageDTO pageDTO, String searchKey, Integer state, String beginDate, String endDate) {
         try {
-//            state = 0;
             PageHelper.startPage(pageDTO.getPageNum(), pageDTO.getPageSize());
-            List<WithdrawDeposit> list = iWithdrawDepositMapper.getAllWithdraw(state, beginDate, endDate);
-            PageInfo pageResult = new PageInfo(list);
-            LOG.info(" getAllWithdraw state:" + state + " " + beginDate + " " + endDate);
-            String strBeginDate = DateUtil.dateToString(DateUtil.getMondayOfThisWeek(), "yyyy-MM-dd HH:mm:ss");
-            String strEndDate = DateUtil.dateToString(DateUtil.getSundayOfThisWeek(), "yyyy-MM-dd HH:mm:ss");
-//            LOG.info("本周一 ：" + strBeginDate + "本周日 ：" + strEndDate);
-            //state  0未处理,1同意（成功） 2不同意(驳回)
-            Example example = new Example(WithdrawDeposit.class);
-            example.createCriteria().andBetween(WithdrawDeposit.CREATE_DATE, strBeginDate, strEndDate);
-            int curWeekAddSize = iWithdrawDepositMapper.selectCountByExample(example);
-            example = new Example(WithdrawDeposit.class);
-            example.createCriteria().andBetween(WithdrawDeposit.CREATE_DATE, strBeginDate, strEndDate)
-                    .andEqualTo(WithdrawDeposit.STATE, 1);
-            int curWeekSuccessSize = iWithdrawDepositMapper.selectCountByExample(example);
-
-            example = new Example(WithdrawDeposit.class);
-            example.createCriteria()
-                    .andBetween(WithdrawDeposit.CREATE_DATE, strBeginDate, strEndDate)
-                    .andEqualTo(WithdrawDeposit.STATE, 0);
-            int curWeekNoHandleSize = iWithdrawDepositMapper.selectCountByExample(example);
-
-
-            example = new Example(WithdrawDeposit.class);
-            example.createCriteria()
-                    .andEqualTo(WithdrawDeposit.STATE, 0);
-            int allNoHandleSize = iWithdrawDepositMapper.selectCountByExample(example);
-            LOG.info("getAllWithdraw size：" + curWeekAddSize + " " + curWeekSuccessSize +
-                    " " + curWeekNoHandleSize + " " + allNoHandleSize);
-            List<WebWithdrawDTO> webWithdrawDTOlist = new ArrayList<>();
-            for (WithdrawDeposit withdrawDeposit : list) {
-                WebWithdrawDTO webWithdrawDTO = new WebWithdrawDTO();
-                webWithdrawDTO.setId(withdrawDeposit.getId());
-                webWithdrawDTO.setBankName(withdrawDeposit.getBankName());
-                Member member = iMemberMapper.selectByPrimaryKey(withdrawDeposit.getWorkerId());
-                webWithdrawDTO.setMobile(member.getMobile());
-                webWithdrawDTO.setCardNumber(withdrawDeposit.getCardNumber());
-                webWithdrawDTO.setCreateDate(withdrawDeposit.getCreateDate());
-                webWithdrawDTO.setModifyDate(withdrawDeposit.getModifyDate());
-                webWithdrawDTO.setMoney(withdrawDeposit.getMoney());
-                webWithdrawDTO.setName(withdrawDeposit.getName());
-                webWithdrawDTO.setProcessingDate(withdrawDeposit.getProcessingDate());
-                webWithdrawDTO.setState(withdrawDeposit.getState());
-                webWithdrawDTO.setWorkerId(withdrawDeposit.getWorkerId());
-                /*****************************统计********************************/
-                webWithdrawDTO.setCurWeekAddNum(curWeekAddSize);//本周新增
-                webWithdrawDTO.setCurWeekSuccessNum(curWeekSuccessSize);//本周 成功的
-                webWithdrawDTO.setCurWeekNoHandleNum(curWeekNoHandleSize);//本周 待处理的
-                webWithdrawDTO.setAllNoHandleNum(allNoHandleSize);//所有待处理的
-                webWithdrawDTOlist.add(webWithdrawDTO);
+            List<WebWithdrawDTO> withdrawDTOList = iWithdrawDepositMapper.getWebWithdrawList(state, searchKey, beginDate, endDate);
+            PageInfo pageResult = new PageInfo(withdrawDTOList);
+            String imageAddress = configUtil.getValue(SysConfig.DANGJIA_IMAGE_LOCAL, String.class);
+            for (WebWithdrawDTO webWithdrawDTO : withdrawDTOList) {
+                if (!CommonUtil.isEmpty(webWithdrawDTO.getImage())) {
+                    webWithdrawDTO.setImage(imageAddress + webWithdrawDTO.getImage());
+                }
             }
-            pageResult.setList(webWithdrawDTOlist);
+            pageResult.setList(withdrawDTOList);
             return ServerResponse.createBySuccess("查询成功", pageResult);
         } catch (Exception e) {
             e.printStackTrace();
