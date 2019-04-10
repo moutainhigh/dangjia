612a68c1c81c10ffdb8c3d8a64c15e3453f6e211
zengcheng <zengcheng@qq.com>
2019-04-10 10:27:07 +0800

    ..

--- dangjia-group/dangjia-master/dangjia-api-master/src/main/java/com/dangjia/acg/modle/house/MaterialRecord.java
+++ dangjia-group/dangjia-master/dangjia-api-master/src/main/java/com/dangjia/acg/modle/house/MaterialRecord.java
@@ -1,6 +1,7 @@
 package com.dangjia.acg.modle.house;
 
 import com.dangjia.acg.common.annotation.Desc;
+import com.dangjia.acg.common.model.BaseEntity;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -20,7 +21,7 @@ import javax.persistence.Table;
 @Table(name = "dj_house_material_record")
 @ApiModel(description = "管家材料登记")
 @FieldNameConstants(prefix = "")
-public class MaterialRecord {
+public class MaterialRecord extends BaseEntity {
     @Column(name = "house_id")
     @Desc(value = "房子ID")
     @ApiModelProperty("房子ID")
---
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/mapper/house/IMaterialRecordMapper.java
@@ -0,0 +1,14 @@
+package com.dangjia.acg.mapper.house;
+
+import com.dangjia.acg.modle.house.MaterialRecord;
+import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * author: Ronalcheng
+ * Date: 2019/4/10 0010
+ * Time: 9:48
+ */
+@Repository
+public interface IMaterialRecordMapper extends Mapper<MaterialRecord> {
+}
