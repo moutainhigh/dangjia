0681b0f723dd27c91a7d6e16e5791226acf650fb
史伟成 <shiweicheng@zhuku.co>
2019-04-29 15:58:26 +0800

    修改量房图片上传和工匠完工申请后应显示停工的BUG，

--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/core/CraftsmanConstructionService.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/core/CraftsmanConstructionService.java
@@ -128,6 +128,22 @@ public class CraftsmanConstructionService {
         List<ConstructionByWorkerIdBean.ButtonListBean> buttonList = new ArrayList<>();
         if (house.getDecorationType() != 2 && house.getDesignerOk() == 1) {
             buttonList.add(getButton("去量房", 2));
+        }else{
+            switch (house.getDesignerOk()) {
+                case 1://1已支付-设计师待量房
+                case 9://9量房图发给业主
+                    buttonList.add(getButton("上传平面图", 3));
+                    break;
+                case 6://6平面图审核不通过（NG，可编辑平面图）
+                    buttonList.add(getButton("修改平面图", 3));
+                    break;
+                case 7://7通过平面图待发施工图（OK，可编辑施工图）
+                    buttonList.add(getButton("上传施工图", 4));
+                    break;
+                case 8://8施工图片审核不通过（NG，可编辑施工图）
+                    buttonList.add(getButton("修改施工图", 4));
+                    break;
+            }
         }
         bean.setButtonList(buttonList);
         return ServerResponse.createBySuccess("获取施工列表成功！", bean);
