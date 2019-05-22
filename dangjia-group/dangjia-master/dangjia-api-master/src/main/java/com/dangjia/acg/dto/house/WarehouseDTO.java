eeb9aa849f865718fbc89f200b64a77b45a998a3
qiyuxiang <qiyuxiangjava@qq.com>
2019-05-22 18:08:59 +0800

    修复

--- dangjia-group/dangjia-master/dangjia-api-master/src/main/java/com/dangjia/acg/dto/house/WarehouseDTO.java
+++ dangjia-group/dangjia-master/dangjia-api-master/src/main/java/com/dangjia/acg/dto/house/WarehouseDTO.java
@@ -18,6 +18,7 @@ public class WarehouseDTO {
     private double surCount;//仓库剩余 = shopCount - askCount - backCount
     private String productName;
     private double price;
+    private Integer maket;//是否上架  0:未上架；1已上架
     private double tolPrice; //总计价 = realCount * price
     private String unitName;
     private int productType;// 0：材料；1：服务
