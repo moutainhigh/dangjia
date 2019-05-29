c237e93dfc58c41eac970bd211947c2b517ef789
史伟成 <shiweicheng@zhuku.co>
2019-05-29 13:59:46 +0800

    设计修改收费任务30%

--- dangjia-group/dangjia-master/dangjia-api-master/src/main/java/com/dangjia/acg/dto/design/DesignListDTO.java
+++ dangjia-group/dangjia-master/dangjia-api-master/src/main/java/com/dangjia/acg/dto/design/DesignListDTO.java
@@ -4,6 +4,7 @@ import com.dangjia.acg.dto.core.ButtonListBean;
 import com.dangjia.acg.modle.design.QuantityRoomImages;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,8 +15,17 @@ import java.util.List;
  * @date on 2019/5/28 6:06 PM
  */
 @Data
-public class DesignDTO {
+public class DesignListDTO {
     private List<QuantityRoomImages> data;
     private int historyRecord;//是否暂时历史记录
+    private String message;//头部提示信息
     private List<ButtonListBean> buttonList;//按钮
+
+    public DesignListDTO() {
+        buttonList = new ArrayList<>();
+    }
+
+    public void addButton(ButtonListBean button) {
+        buttonList.add(button);
+    }
 }
