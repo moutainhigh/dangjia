e184472eb095b653b092c4c3ca04657887b615c5
qiyuxiang <qiyuxiangjava@qq.com>
2019-06-19 14:27:48 +0800

    业主装修前接口（70%）

---
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/controller/home/MyHomeController.java
@@ -0,0 +1,22 @@
+package com.dangjia.acg.controller.home;
+
+import com.dangjia.acg.api.home.MyHomeAPI;
+import com.dangjia.acg.common.annotation.ApiMethod;
+import com.dangjia.acg.common.response.ServerResponse;
+import com.dangjia.acg.service.house.MyHouseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyHomeController implements MyHomeAPI {
+
+    @Autowired
+    private MyHouseService myHouseService;
+
+    @Override
+    @ApiMethod
+    public ServerResponse getMyHouse(String userToken, String cityId) {
+        return myHouseService.getMyHouse(userToken, cityId);
+    }
+
+}
