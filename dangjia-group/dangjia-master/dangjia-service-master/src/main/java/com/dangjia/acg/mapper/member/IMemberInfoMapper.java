5ba6fcdd11abb5540e26c73e970518207d94d848
wukang <1499913860@qq.com>
2019-06-29 15:38:16 +0800

    业主退货性质

--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/mapper/member/IMemberInfoMapper.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/mapper/member/IMemberInfoMapper.java
@@ -1,8 +1,11 @@
 package com.dangjia.acg.mapper.member;
 
 import com.dangjia.acg.modle.member.MemberInfo;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
 
 /**
  * 用户表dao层
@@ -14,5 +17,11 @@ import tk.mybatis.mapper.common.Mapper;
 public interface IMemberInfoMapper extends Mapper<MemberInfo> {
 
 
+    /**
+     * 获取用户app应用角色
+     * @param memberId
+     * @return
+     */
+    List<MemberInfo> queryUserRole(@Param("memberId") String memberId);
 }
 
