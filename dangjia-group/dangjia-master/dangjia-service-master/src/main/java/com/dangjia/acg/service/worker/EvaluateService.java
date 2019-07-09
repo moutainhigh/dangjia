14a51148992b4f85ba4949dbb6814cbb2e3452b6
qiyuxiang <qiyuxiangjava@qq.com>
2019-07-09 15:24:49 +0800

    组织架构

--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/worker/EvaluateService.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/worker/EvaluateService.java
@@ -16,22 +16,30 @@ import com.dangjia.acg.dao.ConfigUtil;
 import com.dangjia.acg.dto.worker.WorkIntegralDTO;
 import com.dangjia.acg.mapper.core.IHouseFlowApplyMapper;
 import com.dangjia.acg.mapper.core.IHouseFlowMapper;
+import com.dangjia.acg.mapper.core.IHouseWorkerOrderMapper;
 import com.dangjia.acg.mapper.core.IWorkerTypeMapper;
+import com.dangjia.acg.mapper.house.IHouseAccountsMapper;
 import com.dangjia.acg.mapper.house.IHouseMapper;
 import com.dangjia.acg.mapper.house.IMaterialRecordMapper;
 import com.dangjia.acg.mapper.matter.ITechnologyRecordMapper;
 import com.dangjia.acg.mapper.member.IMemberMapper;
 import com.dangjia.acg.mapper.repair.IChangeOrderMapper;
+import com.dangjia.acg.mapper.safe.IWorkerTypeSafeMapper;
+import com.dangjia.acg.mapper.safe.IWorkerTypeSafeOrderMapper;
 import com.dangjia.acg.mapper.worker.IEvaluateMapper;
 import com.dangjia.acg.mapper.worker.IWorkIntegralMapper;
 import com.dangjia.acg.mapper.worker.IWorkerDetailMapper;
 import com.dangjia.acg.modle.basics.Product;
 import com.dangjia.acg.modle.core.HouseFlow;
 import com.dangjia.acg.modle.core.HouseFlowApply;
+import com.dangjia.acg.modle.core.HouseWorkerOrder;
 import com.dangjia.acg.modle.core.WorkerType;
 import com.dangjia.acg.modle.house.House;
+import com.dangjia.acg.modle.house.HouseAccounts;
 import com.dangjia.acg.modle.house.MaterialRecord;
 import com.dangjia.acg.modle.member.Member;
+import com.dangjia.acg.modle.safe.WorkerTypeSafe;
+import com.dangjia.acg.modle.safe.WorkerTypeSafeOrder;
 import com.dangjia.acg.modle.worker.Evaluate;
 import com.dangjia.acg.modle.worker.WorkIntegral;
 import com.dangjia.acg.modle.worker.WorkerDetail;
@@ -75,6 +83,14 @@ public class EvaluateService {
     private IHouseFlowMapper houseFlowMapper;
     @Autowired
     private IHouseMapper houseMapper;
+    @Autowired
+    private IWorkerTypeSafeOrderMapper workerTypeSafeOrderMapper;
+    @Autowired
+    private IHouseWorkerOrderMapper houseWorkerOrderMapper;
+    @Autowired
+    private IHouseAccountsMapper houseAccountsMapper;
+    @Autowired
+    private IWorkerTypeSafeMapper workerTypeSafeMapper;
     @Autowired
     private ConfigMessageService configMessageService;
     @Autowired
@@ -388,6 +404,11 @@ public class EvaluateService {
                 HouseFlow hf = houseFlowMapper.selectByPrimaryKey(houseFlowApply.getHouseFlowId());
                 hf.setPause(1);
                 houseFlowMapper.updateByPrimaryKeySelective(hf);
+            }else{
+
+                HouseWorkerOrder hwo = houseWorkerOrderMapper.getByHouseIdAndWorkerTypeId(houseFlowApply.getHouseId(), houseFlowApply.getWorkerTypeId());
+                /*处理保险订单*/
+                this.insurance(hwo, "3");
             }
             //推送工匠审核结果
             //configMessageService.addConfigMessage(null,"gj",houseFlowApply.getWorkerId(),"0","完工申请结果",String.format(DjConstants.PushMessage.STEWARD_APPLY_FINISHED_PASS,house.getHouseName()) ,"5");
@@ -401,7 +422,45 @@ public class EvaluateService {
             return ServerResponse.createByErrorMessage("操作失败");
         }
     }
+    /**
+     * 保险订单
+     */
+    private void insurance(HouseWorkerOrder hwo, String payState) {
+        try {
+            WorkerTypeSafeOrder wtso = workerTypeSafeOrderMapper.getByWorkerTypeId(hwo.getWorkerTypeId(), hwo.getHouseId());
+            if (wtso != null) {
+                wtso.setState(1);  //已支付
+                wtso.setShopDate(new Date());  //设置购买时间
+                workerTypeSafeOrderMapper.updateByPrimaryKeySelective(wtso);
 
+                hwo.setSafePrice(wtso.getPrice());
+                houseWorkerOrderMapper.updateByPrimaryKeySelective(hwo);//记录保险费
+
+                House house = houseMapper.selectByPrimaryKey(hwo.getHouseId());
+                WorkerTypeSafe wts = workerTypeSafeMapper.selectByPrimaryKey(wtso.getWorkerTypeSafeId());
+                if (house.getMoney() == null) {
+                    house.setMoney(new BigDecimal(0));
+                }
+                //记录项目流水 保险
+                HouseAccounts ha = new HouseAccounts();
+                ha.setReason("收入" + wts.getName() + "费用");
+                ha.setMoney(house.getMoney().add(hwo.getSafePrice()));//项目总钱
+                ha.setState(0);//进
+                ha.setPayMoney(hwo.getSafePrice());//本次数额
+                ha.setHouseId(house.getId());
+                ha.setHouseName(house.getHouseName());
+                ha.setMemberId(house.getMemberId());
+                ha.setName("业主支付");
+                ha.setPayment(payState);//统计支付方式
+                houseAccountsMapper.insert(ha);
+                house.setMoney(house.getMoney().add(hwo.getSafePrice()));//累计项目钱
+                houseMapper.updateByPrimaryKeySelective(house);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+        }
+    }
     /**
      * 业主评价管家完工 最后完工
      */
