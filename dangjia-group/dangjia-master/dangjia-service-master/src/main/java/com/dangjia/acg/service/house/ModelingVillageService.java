7b4f1c7c9b4c2c3d2315254d216c2b9e2154ad21
qiyuxiang <qiyuxiangjava@qq.com>
2019-07-10 16:18:06 +0800

    城市返回修复

--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/house/ModelingVillageService.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/house/ModelingVillageService.java
@@ -68,10 +68,16 @@ public class ModelingVillageService {
         example.createCriteria()
                 .andEqualTo(City.STATE, "0");
         List<City> cityList = cityMapper.selectByExample(example);
+        List<Map> cityMaps =new ArrayList<>();
+        for (City city : cityList) {
+            Map map =BeanUtils.beanToMap(city);
+            map.put("cityId",city.getId());
+            cityMaps.add(map);
+        }
         if (cityList.size() <= 0) {
             return ServerResponse.createByErrorCodeMessage(ServerCode.NO_DATA.getCode(), ServerCode.NO_DATA.getDesc());
         }
-        return ServerResponse.createBySuccess("查询列表成功", cityList);
+        return ServerResponse.createBySuccess("查询列表成功", cityMaps);
     }
 
     public ServerResponse getVillageList(HttpServletRequest request, String cityId) {
