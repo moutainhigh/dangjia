e4955756fd2fd732accb59168717fc4206509431
漆宇翔 <qiyuxiangjava@qq.com>
2019-07-27 16:38:00 +0800

    商品表加入城市字段

--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/repair/MendOrderService.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/repair/MendOrderService.java
@@ -269,10 +269,10 @@ public class MendOrderService {
                 return (ServerResponse) object;
             }
             Member member = (Member) object;
-//            House house = houseMapper.selectByPrimaryKey(houseId);
-//            if (house.getVisitState() == 3 || house.getHaveComplete() == 1) {
-//                return ServerResponse.createByErrorMessage("该房子已完工");
-//            }
+            House house = houseMapper.selectByPrimaryKey(houseId);
+            if (house.getVisitState() == 3 || house.getHaveComplete() == 1) {
+                return ServerResponse.createByErrorMessage("该房子已完工");
+            }
             ServerResponse serverResponse = mendChecking(houseId, null, 4);
             if (!serverResponse.isSuccess()) {
                 return ServerResponse.createByErrorMessage(serverResponse.getResultMsg());
