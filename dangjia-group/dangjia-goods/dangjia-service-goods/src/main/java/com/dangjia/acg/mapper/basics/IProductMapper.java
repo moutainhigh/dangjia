a16cd4be442eb0ab9c2ba71f80c801ea0b15651b
漆宇翔 <qiyuxiangjava@qq.com>
2019-07-27 11:36:35 +0800

    批量更新商品名称

--- dangjia-group/dangjia-goods/dangjia-service-goods/src/main/java/com/dangjia/acg/mapper/basics/IProductMapper.java
+++ dangjia-group/dangjia-goods/dangjia-service-goods/src/main/java/com/dangjia/acg/mapper/basics/IProductMapper.java
@@ -84,13 +84,6 @@ public interface IProductMapper extends Mapper<Product> {
 
     Product selectProductByGoodsIdAndBrandIdAndBrandSeriesId(String goodsId, String brandId, String brandSeriesId);
 
-    /*更新商品名称*/
-    void updateProductById(@Param("oldName") String oldName,@Param("newName") String newName,@Param("brandSeriesId") String brandSeriesId,@Param("brandId") String brandId,
-                           @Param("goodsId") String goodsId,@Param("id") String id);
-
-    /*更新单位*/
-    void updateProductByUnitId(@Param("unitName") String unitName,@Param("unitId") String unitId);
-
 
     /*更新单位*/
     void updateProductValueId(@Param("valueId") String valueId);
