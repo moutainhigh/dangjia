3b5927389a49e15dd9d3efd8b9408ba9d85c893c
漆宇翔 <qiyuxiangjava@qq.com>
2019-08-05 10:51:29 +0800

    修改精算

--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/controller/app/house/HouseController.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/controller/app/house/HouseController.java
@@ -162,7 +162,7 @@ public class HouseController implements HouseAPI {
     @ApiMethod
     public ServerResponse queryConstructionRecord(String houseId, String day, String workerType, PageDTO pageDTO) {
 //        return  houseService.queryConstructionRecord(houseId, pageDTO, null);
-        return houseService.queryConstructionRecordAll(houseId, day, workerType, pageDTO);
+        return houseService.queryConstructionRecordAll(houseId,null, day, workerType, pageDTO);
     }
 
     /**
@@ -174,8 +174,14 @@ public class HouseController implements HouseAPI {
      */
     @Override
     @ApiMethod
-    public ServerResponse queryConstructionRecordAll(String houseId, String day, String workerType, PageDTO pageDTO) {
-        return houseService.queryConstructionRecordAll(houseId, day, workerType, pageDTO);
+    public ServerResponse queryConstructionRecordAll(String houseId,String ids, String day, String workerType, PageDTO pageDTO) {
+        return houseService.queryConstructionRecordAll(houseId, ids,day, workerType, pageDTO);
+    }
+
+    @Override
+    @ApiMethod
+    public ServerResponse queryConstructionRecordType(String houseId) {
+        return houseService.queryConstructionRecordType(houseId);
     }
 
     @Override
