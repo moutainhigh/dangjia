f818cb169434202eccbabc67632babfc18a8e658
liujianlin <365955537@qq.com>
2019-08-06 16:38:02 +0800

    客户详情代码提交

--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/sale/rob/RobService.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/sale/rob/RobService.java
@@ -247,6 +247,7 @@ public class RobService {
             //线索阶段查询详情
             Map<String,Object> map = new HashMap<>();
             map.put("id",clueId);
+            map.put("userId",accessToken.getUserId());
             UserInfoDTO userInfoDTO = clueMapper.queryTips(map);
 
             if(!CommonUtil.isEmpty(userInfoDTO)){
