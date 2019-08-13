f6105220e4ff5c6b1e6771bebb010d79c043a2c8
漆宇翔 <qiyuxiangjava@qq.com>
2019-08-13 20:19:38 +0800

    优化保险判断

--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/controller/data/HouseDistributionController.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/controller/data/HouseDistributionController.java
@@ -32,8 +32,8 @@ public class HouseDistributionController implements HouseDistributionAPI {
     @Override
     @ApiMethod
     public ServerResponse getHouseDistribution(HttpServletRequest request, PageDTO pageDTO, HouseDistribution houseDistribution,
-                                               String startDate, String endDate) {
-        return houseDistributionService.getHouseDistribution(request, pageDTO, houseDistribution, startDate, endDate);
+                                               String startDate, String endDate,String searchKey) {
+        return houseDistributionService.getHouseDistribution(request, pageDTO, houseDistribution, startDate, endDate,searchKey);
     }
 
     /**
