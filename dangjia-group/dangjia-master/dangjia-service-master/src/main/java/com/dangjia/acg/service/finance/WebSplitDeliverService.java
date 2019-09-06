f3f719c7adb8a144e61ce561f520b7ff98de25be
qiyuxiang <qiyuxiangjava@qq.com>
2019-09-06 09:59:49 +0800

    施工现场列表统一返回格式

--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/finance/WebSplitDeliverService.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/finance/WebSplitDeliverService.java
@@ -404,7 +404,11 @@ public class WebSplitDeliverService {
                     cityId=house.getCityId();
                 }
                 SupplierProduct supplierProduct = forMasterAPI.getSupplierProduct(cityId, mendDeliver.getSupplierId(), mendMateriel.getProductId());
-                map.put("supCost", supplierProduct.getPrice());
+                if(supplierProduct!=null){
+                    map.put("supCost", supplierProduct.getPrice());
+                }else {
+                    map.put("supCost", mendMateriel.getCost());
+                }
                 mendMaterielsMap.add(map);
             }
             return ServerResponse.createBySuccess("查询成功", mendMaterielsMap);
