1c17aa66d20e7b78dce745bb1f91f79b38fcf6f1
cyf <cyf@fengjiangit.com>
2019-09-11 19:06:24 +0800

    增加

--- dangjia-group/dangjia-master/dangjia-api-master/src/main/java/com/dangjia/acg/api/app/member/MemberCollectAPI.java
+++ dangjia-group/dangjia-master/dangjia-api-master/src/main/java/com/dangjia/acg/api/app/member/MemberCollectAPI.java
@@ -16,26 +16,42 @@ import javax.servlet.http.HttpServletRequest;
 @Api(value = "当家用户收藏接口", description = "当家用户收藏接口")
 public interface MemberCollectAPI {
 
-    @RequestMapping(value = "member/collect/query", method = RequestMethod.POST)
+    @RequestMapping(value = "member/collect/queryHouse", method = RequestMethod.POST)
     @ApiOperation(value = "查询收藏的工地记录", notes = "查询收藏的工地记录")
     ServerResponse queryCollectHouse(@RequestParam("request") HttpServletRequest request,
                                      @RequestParam("userToken") String userToken,
                                      @RequestParam("pageDTO") PageDTO pageDTO);
 
+    @RequestMapping(value = "member/collect/queryGood", method = RequestMethod.POST)
+    @ApiOperation(value = "查询收藏的商品记录", notes = "查询收藏的商品记录")
+    ServerResponse queryCollectGood(@RequestParam("request") HttpServletRequest request,
+                                     @RequestParam("userToken") String userToken,
+                                     @RequestParam("pageDTO") PageDTO pageDTO);
+
+
     @RequestMapping(value = "member/collect/add", method = RequestMethod.POST)
-    @ApiOperation(value = "添加收藏", notes = "添加收藏")
+    @ApiOperation(value = "添加收藏", notes = "添加收藏，conditionType:0->代表收藏房子 1->代表收藏商品")
     ServerResponse addMemberCollect(@RequestParam("request") HttpServletRequest request,
-                                    @RequestParam("houseId") String houseId);
+                                    @RequestParam("houseId") String houseId,@RequestParam("conditionType") String conditionType);
 
     @RequestMapping(value = "member/collect/check", method = RequestMethod.POST)
-    @ApiOperation(value = "检测是否收藏", notes = "检测是否收藏")
+    @ApiOperation(value = "检测是否收藏", notes = "检测是否收藏，conditionType:0->代表收藏房子 1->代表收藏商品")
     ServerResponse isMemberCollect(@RequestParam("request") HttpServletRequest request,
-                                   @RequestParam("houseId") String houseId);
-
+                                   @RequestParam("houseId") String houseId,@RequestParam("conditionType") String conditionType);
 
     @RequestMapping(value = "member/collect/del", method = RequestMethod.POST)
-    @ApiOperation(value = "取消收藏", notes = "取消收藏")
+    @ApiOperation(value = "取消收藏", notes = "取消收藏,conditionType:0->代表收藏房子 1->代表收藏商品")
     ServerResponse delMemberCollect(@RequestParam("request") HttpServletRequest request,
-                                    @RequestParam("houseId") String houseId);
+                                    @RequestParam("houseId") String houseId,@RequestParam("conditionType") String conditionType);
+
+
+    /**
+     * 我的收藏：查看更多工地 需求有问题，待开发
+     */
+
+    /**
+     * 推荐用户点击最多类别的商品，在类别内随机推荐   需求有问题，待开发
+     */
+
 }
 
