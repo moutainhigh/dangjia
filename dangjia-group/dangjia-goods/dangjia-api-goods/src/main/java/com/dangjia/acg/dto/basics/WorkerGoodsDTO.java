6d3238bf58553f51f4f335563b42c25742f4d362
qiyuxiang <qiyuxiangjava@qq.com>
2019-09-24 16:50:28 +0800

    筛选优化

--- dangjia-group/dangjia-goods/dangjia-api-goods/src/main/java/com/dangjia/acg/dto/basics/WorkerGoodsDTO.java
+++ dangjia-group/dangjia-goods/dangjia-api-goods/src/main/java/com/dangjia/acg/dto/basics/WorkerGoodsDTO.java
@@ -58,7 +58,7 @@ public class WorkerGoodsDTO {
     @ApiModelProperty("otherName")
     private String otherName;
 
-    private int productType;//0:材料；1：包工包料
+    private int productType;//2=人工商品
 
     private Double lastPrice;
     private Date lastTime;
