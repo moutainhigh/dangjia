ec3d54227d1b3f0f31970e5bb8754e1fc3fd2c85
史伟成 <shiweicheng@zhuku.co>
2019-09-24 17:37:35 +0800

    抢单添加语音提示

--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/house/HouseService.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/house/HouseService.java
@@ -1949,8 +1949,8 @@ public class HouseService {
                 houseFlow.setWorkType(2);//待抢单
                 houseFlow.setReleaseTime(new Date());//发布时间
                 houseFlowMapper.updateByPrimaryKeySelective(houseFlow);
-                configMessageService.addConfigMessage(null, AppType.GONGJIANG, "wtId3" + houseFlow.getCityId(),
-                        "0", "新的装修订单", DjConstants.PushMessage.SNAP_UP_ORDER, "4");
+                configMessageService.addConfigMessage(AppType.GONGJIANG, "wtId3" + houseFlow.getCityId(),
+                        "新的装修订单", DjConstants.PushMessage.SNAP_UP_ORDER, 4, null, "您有新的装修订单，快去抢吧！");
                 //推送消息给业主等待大管家抢单
                 configMessageService.addConfigMessage(null, AppType.ZHUANGXIU, house.getMemberId(),
                         "0", "等待大管家抢单", String.format(DjConstants.PushMessage.ACTUARIAL_COMPLETION,
