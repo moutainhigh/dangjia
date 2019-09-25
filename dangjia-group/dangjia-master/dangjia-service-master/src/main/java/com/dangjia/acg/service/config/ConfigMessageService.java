500b3dd2db1de73be2f700eda67b7481932d5d85
史伟成 <shiweicheng@zhuku.co>
2019-09-25 17:08:21 +0800

    修改推送TAG

--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/config/ConfigMessageService.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/config/ConfigMessageService.java
@@ -16,6 +16,7 @@ import com.dangjia.acg.modle.member.Member;
 import com.dangjia.acg.service.core.CraftsmanConstructionService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -45,7 +46,6 @@ public class ConfigMessageService {
     /**
      * 获取所有公共消息(web端列表)
      *
-     * @param configMessage
      * @return
      */
     public ServerResponse queryConfigMessages(HttpServletRequest request, PageDTO pageDTO) {
@@ -82,9 +82,9 @@ public class ConfigMessageService {
             criteria.andCondition("(" +
                     "target_uid='" + member.getId() + "' or " +
                     "target_uid='" + cityId + "' or " +
-                    "target_uid='wtId" + member.getWorkerTypeId() + "' or " +
+                    "target_uid='" + DigestUtils.md5Hex("wtId" + member.getWorkerTypeId()) + "' or " +
                     "target_type=1  or " +
-                    "target_uid='wtId" + member.getWorkerTypeId() + cityId + "'" +
+                    "target_uid='" + DigestUtils.md5Hex("wtId" + member.getWorkerTypeId() + cityId) + "'" +
                     ")");
 //            criteria.andEqualTo("targetUid", accessToken.getMember().getId()).orEqualTo("targetType","1");
         } else {
--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/core/HouseWorkerService.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/core/HouseWorkerService.java
@@ -46,6 +46,7 @@ import com.dangjia.acg.service.house.HouseService;
 import com.dangjia.acg.service.worker.EvaluateService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -1031,7 +1032,8 @@ public class HouseWorkerService {
             houseFlow.setWorkType(2);
             houseFlow.setReleaseTime(new Date());//发布时间
             houseFlowMapper.updateByPrimaryKeySelective(houseFlow);
-            configMessageService.addConfigMessage(AppType.GONGJIANG, "wtId" + houseFlow.getWorkerType() + houseFlow.getCityId(),
+            configMessageService.addConfigMessage(AppType.GONGJIANG,
+                    DigestUtils.md5Hex("wtId" + houseFlow.getWorkerType() + houseFlow.getCityId()),
                     "新的装修订单", DjConstants.PushMessage.SNAP_UP_ORDER, 4, null, "您有新的装修订单，快去抢吧！");
             return ServerResponse.createBySuccessMessage("提前进场成功");
         } catch (Exception e) {
