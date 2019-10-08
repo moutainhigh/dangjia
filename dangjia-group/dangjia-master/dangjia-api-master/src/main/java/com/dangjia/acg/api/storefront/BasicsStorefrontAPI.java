b1cae84706bdff446a7f81a60a2622b792f533b9
cyf <cyf@fengjiangit.com>
2019-10-08 18:50:30 +0800

    优化购物车接口  20190928  chenyufeng

---
+++ dangjia-group/dangjia-master/dangjia-api-master/src/main/java/com/dangjia/acg/api/storefront/BasicsStorefrontAPI.java
@@ -0,0 +1,23 @@
+package com.dangjia.acg.api.storefront;
+
+import com.dangjia.acg.common.response.ServerResponse;
+import com.dangjia.acg.model.storefront.Storefront;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+/**
+ * chenyufeng  2019-10-08  店铺管理
+ */
+@Api(description = "店铺管理接口")
+@FeignClient("dangjia-service-goods")
+public interface BasicsStorefrontAPI {
+
+    @PostMapping("/web/addStorefront")
+    @ApiOperation(value = "新增店铺信息", notes = "新增店铺信息")
+    ServerResponse addStorefront(@RequestParam("userToken") String userToken, Storefront  storefront);
+
+}
---
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/storefront/StorefrontService.java
@@ -0,0 +1,56 @@
+package com.dangjia.acg.service.storefront;
+
+import cn.jiguang.common.utils.StringUtils;
+import com.dangjia.acg.common.response.ServerResponse;
+import com.dangjia.acg.mapper.storefront.IStorefrontMapper;
+import com.dangjia.acg.model.storefront.Storefront;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StorefrontService {
+
+    /**
+     * 声明日志
+     */
+    private static Logger logger = LoggerFactory.getLogger(StorefrontService.class);
+
+    @Autowired
+    private IStorefrontMapper istorefrontMapper;
+
+    public ServerResponse addStorefront(String userToken, Storefront storefront) {
+
+        try {
+            //店铺名称不能大于10个字
+            String storefrontName=storefront.getStorefrontName();
+            if(storefrontName.length()>10)
+            {
+                ServerResponse.createByErrorMessage("店铺名称不能大于10个字!");
+            }
+            //店铺地址限制字数30个字，支持字母、数字、汉字
+            String storefrontAddress=storefront.getStorefrontAddress();
+            if(storefrontAddress.length()>30)
+            {
+                ServerResponse.createByErrorMessage("店铺地址不能大于30个字!");
+            }
+            //店铺介绍限制字数20个字，支持字母、数字、汉字
+            String storefrontDesc=storefront.getStorefrontDesc();
+            if(storefrontDesc.length()>20)
+            {
+                ServerResponse.createByErrorMessage("店铺介绍不能大于20个字!");
+            }
+            int i = istorefrontMapper.insert(storefront);
+            if (i > 0) {
+                ServerResponse.createBySuccessMessage("新增成功!");
+            } else {
+                ServerResponse.createBySuccessMessage("新增失败!");
+            }
+        } catch (Exception e) {
+            logger.error("查询失败：", e);
+            return ServerResponse.createByErrorMessage("查询失败");
+        }
+        return null;
+    }
+}
