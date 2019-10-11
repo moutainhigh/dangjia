35b3684be2664a49fd410415e0431ea419086067
cyf <cyf@fengjiangit.com>
2019-10-11 10:42:33 +0800

    店铺管理  2019.10.8  chenyufeng

--- dangjia-group/dangjia-shop/dangjia-api-shop/src/main/java/com/dangjia/acg/api/StorefrontProductAPI.java
+++ dangjia-group/dangjia-shop/dangjia-api-shop/src/main/java/com/dangjia/acg/api/StorefrontProductAPI.java
@@ -14,32 +14,32 @@ public interface StorefrontProductAPI {
 
     @PostMapping("/web/queryStorefrontProductByType")
     @ApiOperation(value = "通过类别查询商品", notes = "通过类别查询商品")
-    ServerResponse queryStorefrontProductByType(@RequestParam("userToken") String userToken, Storefront storefront);
+    ServerResponse queryStorefrontProductByType(@RequestParam("userToken") String userToken, @RequestParam("type") String type);
 
 
-    @PostMapping("/web/setStoreProductByIsShelfStatus")
+    @PostMapping("/web/setSpStatusById")
     @ApiOperation(value = "设置商品上下架", notes = "设置商品上下架")
-    ServerResponse setSpStatusById(@RequestParam("userToken") String userToken, @RequestParam("id") String id,@RequestParam("isShelfStatus") String isShelfStatus );
+    ServerResponse setSpStatusById(@RequestParam("userToken") String userToken, @RequestParam("id") String id, @RequestParam("isShelfStatus") String isShelfStatus);
 
 
     @PostMapping("/web/setAllStoreProductByIsShelfStatus")
     @ApiOperation(value = "设置商品批量架", notes = "设置商品批量架")
-    ServerResponse setAllStoreProductByIsShelfStatus(@RequestParam("userToken") String userToken, @RequestParam("id") String id,@RequestParam("isShelfStatus") String isShelfStatus );
+    ServerResponse setAllStoreProductByIsShelfStatus(@RequestParam("userToken") String userToken, @RequestParam("isShelfStatus") String isShelfStatus);
 
 
     @PostMapping("/web/delStorefrontProductById")
     @ApiOperation(value = "根据主键删除商品", notes = "根据主键删除商品")
-    ServerResponse delStorefrontProductById(@RequestParam("userToken") String userToken,@RequestParam("id") String id );
+    ServerResponse delStorefrontProductById(@RequestParam("userToken") String userToken, @RequestParam("id") String id);
 
 
     @PostMapping("/web/updateStorefrontProductById")
     @ApiOperation(value = "根据id修改店铺商品", notes = "根据id修改店铺商品")
-    ServerResponse updateStorefrontProductById(@RequestParam("userToken") String userToken,@RequestParam("id") String id );
+    ServerResponse updateStorefrontProductById(@RequestParam("userToken") String userToken, @RequestParam("id") String id);
 
 
     @PostMapping("/web/queryStorefrontProductBykey")
     @ApiOperation(value = "查询已选列表", notes = "查询已选列表")
-    ServerResponse queryStorefrontProductBykey(@RequestParam("userToken") String userToken,@RequestParam("key") String key );
+    ServerResponse queryStorefrontProductBykey(@RequestParam("userToken") String userToken, @RequestParam("key") String key);
 
 
 }
--- dangjia-group/dangjia-shop/dangjia-service-shop/src/main/java/com/dangjia/acg/controller/storefront/StorefrontProductController.java
+++ dangjia-group/dangjia-shop/dangjia-service-shop/src/main/java/com/dangjia/acg/controller/storefront/StorefrontProductController.java
@@ -4,7 +4,10 @@ import com.dangjia.acg.api.StorefrontProductAPI;
 import com.dangjia.acg.common.annotation.ApiMethod;
 import com.dangjia.acg.common.response.ServerResponse;
 import com.dangjia.acg.modle.storefront.Storefront;
+import com.dangjia.acg.service.storefront.StorefrontProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @ClassName: StorefrontController
  * @Description: 店铺商品管理接口类
@@ -14,39 +17,46 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StorefrontProductController implements StorefrontProductAPI {
 
+    @Autowired
+    private StorefrontProductService storefrontProductService;
+
+
     @Override
     @ApiMethod
-    public ServerResponse queryStorefrontProductByType(String userToken, Storefront storefront) {
-        return null;
+    public ServerResponse queryStorefrontProductByType(String userToken, String type) {
+        return storefrontProductService.queryStorefrontProductByType(userToken, type);
     }
 
     @Override
     @ApiMethod
     public ServerResponse setSpStatusById(String userToken, String id, String isShelfStatus) {
-        return null;
+        return storefrontProductService.setSpStatusById(userToken, id, isShelfStatus);
     }
 
     @Override
     @ApiMethod
-    public ServerResponse setAllStoreProductByIsShelfStatus(String userToken, String id, String isShelfStatus) {
-        return null;
+    public ServerResponse setAllStoreProductByIsShelfStatus(String userToken, String isShelfStatus) {
+        return storefrontProductService.setAllStoreProductByIsShelfStatus(userToken, isShelfStatus);
     }
 
     @Override
     @ApiMethod
     public ServerResponse delStorefrontProductById(String userToken, String id) {
-        return null;
+
+        return storefrontProductService.delStorefrontProductById(userToken, id);
     }
 
     @Override
     @ApiMethod
     public ServerResponse updateStorefrontProductById(String userToken, String id) {
-        return null;
+
+        return storefrontProductService.updateStorefrontProductById(userToken, id);
     }
 
     @Override
     @ApiMethod
     public ServerResponse queryStorefrontProductBykey(String userToken, String key) {
-        return null;
+
+        return storefrontProductService.queryStorefrontProductBykey(userToken, key);
     }
 }
