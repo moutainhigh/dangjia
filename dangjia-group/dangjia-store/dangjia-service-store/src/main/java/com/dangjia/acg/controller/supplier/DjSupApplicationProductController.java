cf032cc612b38995a91ee3ebf4ae1dbebf341539
cyf <cyf@fengjiangit.com>
2019-10-11 13:47:30 +0800

    店铺管理  2019.10.8  chenyufeng

---
+++ dangjia-group/dangjia-store/dangjia-service-store/src/main/java/com/dangjia/acg/controller/supplier/DjSupApplicationProductController.java
@@ -0,0 +1,9 @@
+package com.dangjia.acg.controller.supplier;
+
+import com.dangjia.acg.api.supplier.DjSupApplicationProductAPI;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DjSupApplicationProductController implements DjSupApplicationProductAPI {
+
+}
---
+++ dangjia-group/dangjia-store/dangjia-service-store/src/main/java/com/dangjia/acg/mapper/supplier/DjSupApplicationProductMapper.java
@@ -0,0 +1,17 @@
+package com.dangjia.acg.mapper.supplier;
+
+        import com.dangjia.acg.modle.supplier.DjSupApplication;
+        import com.dangjia.acg.modle.supplier.DjSupApplicationProduct;
+        import org.springframework.stereotype.Repository;
+        import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * Created with IntelliJ IDEA.
+ * author: wk
+ * Date: 10/10/2019
+ * Time: 下午 3:44
+ */
+@Repository
+public interface DjSupApplicationProductMapper extends Mapper<DjSupApplicationProduct> {
+
+}
