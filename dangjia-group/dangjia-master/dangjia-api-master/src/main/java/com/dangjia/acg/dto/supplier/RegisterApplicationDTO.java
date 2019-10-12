3e3c2611e0865ec3780cf36a9cf1a6f852a8a82b
fangzhenghong <fzhdao@163.com>
2019-10-12 15:23:52 +0800

    增加审核注册列表

---
+++ dangjia-group/dangjia-master/dangjia-api-master/src/main/java/com/dangjia/acg/dto/supplier/RegisterApplicationDTO.java
@@ -0,0 +1,52 @@
+package com.dangjia.acg.dto.supplier;
+
+import com.dangjia.acg.dto.sale.achievement.AchievementInfoDTO;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ *门店业绩 返回参数
+ */
+@Data
+public class RegisterApplicationDTO implements Serializable {
+
+    @ApiModelProperty("id")
+    private String id;//申请ID
+
+    @ApiModelProperty("城市id")
+    private String cityId;//城市ID
+
+    @ApiModelProperty("城市名称")
+    private String cityName;//城市名称
+
+    @ApiModelProperty("姓名")
+    private String name;//姓名
+
+    @ApiModelProperty("联系电话")
+    private String mobile;//联系电话
+
+    @ApiModelProperty("身份证号码")
+    private String cardNumber;//身份证号码
+
+    @ApiModelProperty("身份证照片（正、反面）")
+    private String cardImage;//身份证号码
+
+    @ApiModelProperty("营业执照")
+    private String businessLicense;//营业执照
+
+    @ApiModelProperty("申请类型 1:店铺 2:供应商")
+    private String applicationType;//申请类型 1:店铺 2:供应商
+
+    @ApiModelProperty("审核状态 0:审核中 1:通过 2:不通过")
+    private Integer applicationStatus;//审核状态 0:审核中 1:通过 2:不通过
+
+    @ApiModelProperty("失败原因")
+    private String failReason;//失败原因
+
+    @ApiModelProperty("审核人ID")
+    private String auditUserId;//审核人ID
+
+}
--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/mapper/supplier/DjRegisterApplicationMapper.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/mapper/supplier/DjRegisterApplicationMapper.java
@@ -1,8 +1,12 @@
 package com.dangjia.acg.mapper.supplier;
 
+import com.dangjia.acg.dto.supplier.RegisterApplicationDTO;
 import com.dangjia.acg.modle.supplier.DjRegisterApplication;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,4 +16,11 @@ import tk.mybatis.mapper.common.Mapper;
  */
 @Repository
 public interface DjRegisterApplicationMapper extends Mapper<DjRegisterApplication> {
+
+    /**
+     * 查询服务列表
+     * @return
+     */
+    List<RegisterApplicationDTO> getAllRegistList(@Param("applicationStatus") String applicationStatus,
+                                                  @Param("searchKey") String searchKey);
 }
