e846decad1b7501129d83d66cdbb46a779a57cc6
qiyuxiang <qiyuxiangjava@qq.com>
2019-10-14 10:09:39 +0800

    订单状态

--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/pay/PaymentService.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/pay/PaymentService.java
@@ -1013,7 +1013,7 @@ public class PaymentService {
                 if (insurance == null) {
                     return ServerResponse.createByErrorMessage("保险记录不存在");
                 }
-                if (businessOrderList.size() == 0) {
+                if (businessOrderList.size() == 0|| (businessOrder!=null && businessOrder.getState()==4)) {
                     businessOrder = new BusinessOrder();
                     businessOrder.setMemberId(insurance.getWorkerId()); //公众号唯一标识
                     businessOrder.setHouseId(null);
@@ -1041,7 +1041,7 @@ public class PaymentService {
                 if (houseDistribution == null) {
                     return ServerResponse.createByErrorMessage("验房分销记录不存在");
                 }
-                if (businessOrderList.size() == 0) {
+                if (businessOrderList.size() == 0|| (businessOrder!=null && businessOrder.getState()==4)) {
                     businessOrder = new BusinessOrder();
                     businessOrder.setMemberId(houseDistribution.getOpenid()); //公众号唯一标识
                     businessOrder.setHouseId(houseDistribution.getOpenid());
@@ -1071,7 +1071,7 @@ public class PaymentService {
                     return ServerResponse.createByErrorMessage("订单记录不存在");
                 }
                 House house = houseMapper.selectByPrimaryKey(productChangeOrder.getHouseId());
-                if (businessOrderList.size() == 0) {
+                if (businessOrderList.size() == 0|| (businessOrder!=null && businessOrder.getState()==4)) {
                     businessOrder = new BusinessOrder();
                     businessOrder.setMemberId(house.getMemberId()); //公众号唯一标识
                     businessOrder.setHouseId(productChangeOrder.getHouseId());
@@ -1102,7 +1102,7 @@ public class PaymentService {
                 PurchaseOrder purchaseOrder = (PurchaseOrder) datas.get("purchaseOrder");
                 List<FlowActuaryDTO> flowActuaryDTOList = (List<FlowActuaryDTO>) datas.get("list");
                 House house = houseMapper.selectByPrimaryKey(purchaseOrder.getHouseId());
-                if (businessOrderList.size() == 0) {
+                if (businessOrderList.size() == 0|| (businessOrder!=null && businessOrder.getState()==4)) {
                     businessOrder = new BusinessOrder();
                     businessOrder.setMemberId(house.getMemberId()); //公众号唯一标识
                     businessOrder.setHouseId(purchaseOrder.getHouseId());
@@ -1126,7 +1126,7 @@ public class PaymentService {
                 }
                 House house = houseMapper.selectByPrimaryKey(mendOrder.getHouseId());
                 WorkerType workerType = workerTypeMapper.selectByPrimaryKey(mendOrder.getWorkerTypeId());
-                if (businessOrderList.size() == 0) {
+                if (businessOrderList.size() == 0|| (businessOrder!=null && businessOrder.getState()==4)) {
                     businessOrder = new BusinessOrder();
                     businessOrder.setMemberId(house.getMemberId()); //公众号唯一标识
                     businessOrder.setHouseId(mendOrder.getHouseId());
@@ -1215,7 +1215,7 @@ public class PaymentService {
                 }
             }
             House house = houseMapper.selectByPrimaryKey(houseId);
-            if (businessOrderList.size() == 0) {
+            if (businessOrderList.size() == 0|| (businessOrder!=null && businessOrder.getState()==4)) {
                 businessOrder = new BusinessOrder();
                 businessOrder.setMemberId(house.getMemberId());
                 businessOrder.setHouseId(houseId);
