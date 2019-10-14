57413a0d29ccd575a82cd316fde94450198cd878
qiyuxiang <qiyuxiangjava@qq.com>
2019-10-14 15:00:15 +0800

    订单状态

--- dangjia-group/dangjia-master/dangjia-api-master/src/main/java/com/dangjia/acg/dto/deliver/SplitDeliverItemDTO.java
+++ dangjia-group/dangjia-master/dangjia-api-master/src/main/java/com/dangjia/acg/dto/deliver/SplitDeliverItemDTO.java
@@ -1,5 +1,6 @@
 package com.dangjia.acg.dto.deliver;
 
+import com.dangjia.acg.common.annotation.ExcelField;
 import lombok.Data;
 
 /**
@@ -9,18 +10,37 @@ import lombok.Data;
  */
 @Data
 public class SplitDeliverItemDTO {
+
+
+    @ExcelField(titile = "供应商", offset = 1)
+    private String supplierName;//
+    @ExcelField(titile = "工地名称", offset = 2)
+    private String houseName;//工地名称
     private String image;//图片
+
+    @ExcelField(titile = "商品名称", offset = 3)
     private String productName;//名字
     private String productSn;//编号
     private Double shopCount;//购买总数
     private Double num;//本次发货数量
+
+    @ExcelField(titile = "单位", offset = 7)
     private String unitName;//单位
     private String brandSeriesName;//品牌系列
     private Double cost;//平均成本价
     private Double price;//销售价
     private Double totalPrice; //销售价*发货数量
     private String id;
+
+    @ExcelField(titile = "收货数量", offset = 4)
     private Double receive;//收货数量
+
+    @ExcelField(titile = "成本单价", offset = 5)
     private Double supCost;//选择的供应商提供的单价
+
+    @ExcelField(titile = "成本总价", offset = 6)
+    private Double supCostTotal;//成本总价
+
+
     private Double askCount;//要货数量
 }
