66bb7505a2337df48c43bb67eac283a6a757a94b
fangzhenghong <fzhdao@163.com>
2019-10-30 20:05:15 +0800

    增加仅退款订单查询及提交仅退款订单接口

--- dangjia-group/dangjia-bill/dangjia-api-bill/src/main/java/com/dangjia/acg/dto/refund/OrderProgressDTO.java
+++ dangjia-group/dangjia-bill/dangjia-api-bill/src/main/java/com/dangjia/acg/dto/refund/OrderProgressDTO.java
@@ -7,16 +7,8 @@ package com.dangjia.acg.dto.refund;
  * Time: 上午 10:00
  */
 
-import com.dangjia.acg.common.annotation.Desc;
-import com.dangjia.acg.common.model.BaseEntity;
-import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import lombok.experimental.FieldNameConstants;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
 
 @Data
 public class OrderProgressDTO {
