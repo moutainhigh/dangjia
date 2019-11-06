1ab2f03a18dba9f608b2e0c4598f3f60eeac5897
wukang <1499913860@qq.com>
2019-11-06 14:51:51 +0800

    修改

--- dangjia-group/dangjia-store/dangjia-api-store/src/main/java/com/dangjia/acg/dto/supplier/DjSupSupplierProductDTO.java
+++ dangjia-group/dangjia-store/dangjia-api-store/src/main/java/com/dangjia/acg/dto/supplier/DjSupSupplierProductDTO.java
@@ -24,13 +24,15 @@ public class DjSupSupplierProductDTO extends BaseEntity {
     private Integer stock;//库存
     private Double supplyPrice;//供应价
     private Double salesPrice;//销售价
-    private String isElevatorFee;//是否收取上楼费
+    private String isCartagePrice;//是否收取上楼费
     private String supplyRelationship;//供应关系 0:供应 1:停供
     private Double adjustPrice;//调后价格
     private Date adjustTime;//调价时间
     private String valueNameArr;
     private String valueIdArr;
     private Double porterage;//搬运费
+    private String supId;
+    private String shopId;
     /**
      * 城市ID
      */
