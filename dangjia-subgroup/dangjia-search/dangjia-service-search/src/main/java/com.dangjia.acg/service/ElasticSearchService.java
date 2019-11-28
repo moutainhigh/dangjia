599a958b16859af6977b4f24fb932d62a9fc9c2b
qiyuxiang <qiyuxiangjava@qq.com>
2019-11-28 15:33:59 +0800

    优化搜索

--- dangjia-subgroup/dangjia-search/dangjia-service-search/src/main/java/com.dangjia.acg/service/ElasticSearchService.java
+++ dangjia-subgroup/dangjia-search/dangjia-service-search/src/main/java/com.dangjia.acg/service/ElasticSearchService.java
@@ -15,7 +15,6 @@ import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequestBuilder;
-import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
@@ -36,19 +35,18 @@ import java.util.*;
 public class ElasticSearchService {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(ElasticSearchService.class);
-  private  TransportClient client = ElasticsearchConfiguration.client;
   private String indexName="dangjia";
 
   public String saveESJson(String jsonStr,String tableTypeName) {
     IndexResponse indexResponse;
     try {
+      LOGGER.info("ES 开始插入"+jsonStr);
       Map map =JSONObject.parseObject(jsonStr);
-      indexResponse = client.prepareIndex(indexName+"_"+tableTypeName.toLowerCase(), tableTypeName).setSource(map).get();
-      LOGGER.info("ES 插入完成");
-    } catch (RuntimeException e) {
-      e.printStackTrace();
-      throw new BaseException(ServerCode.JSON_TYPE_ERROR, "JSON格式不正确，请检查JSON");
+      indexResponse = ElasticsearchConfiguration.client.prepareIndex(indexName+"_"+tableTypeName.toLowerCase(), tableTypeName).setSource(map).get();
+      LOGGER.info("ES 插入完成"+jsonStr);
     } catch (Exception e) {
+      e.printStackTrace();
+      LOGGER.error("ES 插入异常",e);
       throw new BaseException(ServerCode.ES_ERROR, "保存搜索引擎失败");
     }
     return indexResponse.getId();
@@ -67,7 +65,7 @@ public class ElasticSearchService {
     fieldList.toArray(fields);
     try {
       //查询搜索对象
-      SearchRequestBuilder searchRequestBuilder = client.prepareSearch(indexName+"_"+elasticSearchDTO.getTableTypeName().toLowerCase()).setTypes(elasticSearchDTO.getTableTypeName());
+      SearchRequestBuilder searchRequestBuilder = ElasticsearchConfiguration.client.prepareSearch(indexName+"_"+elasticSearchDTO.getTableTypeName().toLowerCase()).setTypes(elasticSearchDTO.getTableTypeName());
       BoolQueryBuilder subCodeQuery = QueryBuilders.boolQuery();
       setParamTerm(subCodeQuery, elasticSearchDTO.getParamMap(),elasticSearchDTO.getNotParamMap());
       if(CommonUtil.isEmpty(elasticSearchDTO.getSearchContent())) {
@@ -94,7 +92,7 @@ public class ElasticSearchService {
     String[] fields = new String[fieldList.size()];
     fieldList.toArray(fields);
     try {
-      SearchRequestBuilder searchRequestBuilder = client.prepareSearch(indexName+"_"+elasticSearchDTO.getTableTypeName().toLowerCase()).setTypes(elasticSearchDTO.getTableTypeName());
+      SearchRequestBuilder searchRequestBuilder = ElasticsearchConfiguration.client.prepareSearch(indexName+"_"+elasticSearchDTO.getTableTypeName().toLowerCase()).setTypes(elasticSearchDTO.getTableTypeName());
       BoolQueryBuilder subCodeQuery = QueryBuilders.boolQuery();
       setParamTerm(subCodeQuery,elasticSearchDTO.getParamMap(),elasticSearchDTO.getNotParamMap());
       if(CommonUtil.isEmpty(elasticSearchDTO.getSearchContent())) {
@@ -102,7 +100,7 @@ public class ElasticSearchService {
       }else{
         subCodeQuery.must(QueryBuilders.multiMatchQuery(elasticSearchDTO.getSearchContent(), fields).slop(1));
       }
-        //查询
+      //查询
       searchRequestBuilder.setQuery(subCodeQuery);
 
       return searchResponse(searchRequestBuilder,elasticSearchDTO.getSortMap(),elasticSearchDTO.getPageDTO());
@@ -127,7 +125,7 @@ public class ElasticSearchService {
   public JSONObject getSearchJsonId( String tableTypeName,String prepareId){
     List<JSONObject> strings = new ArrayList<JSONObject>();
     try {
-      SearchRequestBuilder searchRequestBuilder = client.prepareSearch(indexName+"_"+tableTypeName.toLowerCase()).setQuery(QueryBuilders.termsQuery("id",prepareId)).setTypes(tableTypeName);
+      SearchRequestBuilder searchRequestBuilder = ElasticsearchConfiguration.client.prepareSearch(indexName+"_"+tableTypeName.toLowerCase()).setQuery(QueryBuilders.termsQuery("id",prepareId)).setTypes(tableTypeName);
       strings = searchResponse(searchRequestBuilder,null);
     } catch (Exception e) {
       LOGGER.error(e.getMessage());
@@ -155,28 +153,28 @@ public class ElasticSearchService {
    * @param searchRequestBuilder
    * @return
    */
-   public List<JSONObject> searchResponse(SearchRequestBuilder searchRequestBuilder,Map<String,Integer> sortMap){
-     //拼接排序规则
-     setSortTerm(searchRequestBuilder,sortMap);
-     LOGGER.debug("searchRequestBuilder不分页:"+searchRequestBuilder);
-     //分页
-     searchRequestBuilder.setSize(1000);
-     SearchResponse response = searchRequestBuilder.get();
+  public List<JSONObject> searchResponse(SearchRequestBuilder searchRequestBuilder,Map<String,Integer> sortMap){
+    //拼接排序规则
+    setSortTerm(searchRequestBuilder,sortMap);
+    LOGGER.debug("searchRequestBuilder不分页:"+searchRequestBuilder);
+    //分页
+    searchRequestBuilder.setSize(1000);
+    SearchResponse response = searchRequestBuilder.get();
 
-     List<JSONObject> arrList = new ArrayList<JSONObject>();
-     SearchHits hits = response.getHits();
-     if (null != hits && hits.getTotalHits() > 0) {
-       for (SearchHit hit : hits) {
-         String json = hit.getSourceAsString();
-         JSONObject jsonObject=JSON.parseObject(json);
-         jsonObject.put("esId",hit.getId());
-         arrList.add(jsonObject);
-       }
-     } else {
-       LOGGER.info("没有查询到任何结果！");
-     }
-     return arrList;
-   }
+    List<JSONObject> arrList = new ArrayList<JSONObject>();
+    SearchHits hits = response.getHits();
+    if (null != hits && hits.getTotalHits() > 0) {
+      for (SearchHit hit : hits) {
+        String json = hit.getSourceAsString();
+        JSONObject jsonObject=JSON.parseObject(json);
+        jsonObject.put("esId",hit.getId());
+        arrList.add(jsonObject);
+      }
+    } else {
+      LOGGER.info("没有查询到任何结果！");
+    }
+    return arrList;
+  }
 
   /**
    * 处理查询数据（分页）
@@ -184,47 +182,47 @@ public class ElasticSearchService {
    * @param pageDTO
    * @return
    */
-   public PageInfo searchResponse(SearchRequestBuilder searchRequestBuilder,Map<String,Integer> sortMap,PageDTO pageDTO){
-     //拼接排序规则
-     setSortTerm(searchRequestBuilder,sortMap);
-     //分页
-     searchRequestBuilder.setFrom(pageDTO.getPageNum()-1).setSize(pageDTO.getPageSize());
-     LOGGER.debug("searchRequestBuilder分页:"+searchRequestBuilder);
-     SearchResponse response = searchRequestBuilder.get();
-     List<JSONObject> arrList = new ArrayList<JSONObject>();
-     SearchHits hits = response.getHits();
-     if (null != hits && hits.getTotalHits() > 0) {
-       for (SearchHit hit : hits) {
-         String json = hit.getSourceAsString();
-         JSONObject jsonObject=JSON.parseObject(json);
-         jsonObject.put("esId",hit.getId());
-         arrList.add(jsonObject);
-       }
-     } else {
-       LOGGER.info("没有查询到任何结果！");
-     }
-     PageInfo pageBean = new PageInfo(arrList);
-     pageBean.setList(arrList);
-     pageBean.setTotal(hits.getTotalHits());
-     pageBean.setPages(Integer.valueOf((int) hits.getTotalHits())/pageDTO.getPageSize());
-     pageBean.setPageNum(pageDTO.getPageNum());
-     pageBean.setPageSize(pageDTO.getPageSize());
-     return pageBean;
-   }
+  public PageInfo searchResponse(SearchRequestBuilder searchRequestBuilder,Map<String,Integer> sortMap,PageDTO pageDTO){
+    //拼接排序规则
+    setSortTerm(searchRequestBuilder,sortMap);
+    //分页
+    searchRequestBuilder.setFrom(pageDTO.getPageNum()-1).setSize(pageDTO.getPageSize());
+    LOGGER.debug("searchRequestBuilder分页:"+searchRequestBuilder);
+    SearchResponse response = searchRequestBuilder.get();
+    List<JSONObject> arrList = new ArrayList<JSONObject>();
+    SearchHits hits = response.getHits();
+    if (null != hits && hits.getTotalHits() > 0) {
+      for (SearchHit hit : hits) {
+        String json = hit.getSourceAsString();
+        JSONObject jsonObject=JSON.parseObject(json);
+        jsonObject.put("esId",hit.getId());
+        arrList.add(jsonObject);
+      }
+    } else {
+      LOGGER.info("没有查询到任何结果！");
+    }
+    PageInfo pageBean = new PageInfo(arrList);
+    pageBean.setList(arrList);
+    pageBean.setTotal(hits.getTotalHits());
+    pageBean.setPages(Integer.valueOf((int) hits.getTotalHits())/pageDTO.getPageSize());
+    pageBean.setPageNum(pageDTO.getPageNum());
+    pageBean.setPageSize(pageDTO.getPageSize());
+    return pageBean;
+  }
 
   /**
    * 删除
    * @param tableTypeName
    * @param prepareId
    */
-   public void deleteResponse( String tableTypeName,String prepareId){
-     String eid=getESId(tableTypeName,prepareId);
-     if(!CommonUtil.isEmpty(eid)) {
-       DeleteRequestBuilder deleteResponse = client.prepareDelete(indexName+"_"+tableTypeName.toLowerCase(), tableTypeName, eid);
-       deleteResponse.execute().actionGet();
-     }
+  public void deleteResponse( String tableTypeName,String prepareId){
+    String eid=getESId(tableTypeName,prepareId);
+    if(!CommonUtil.isEmpty(eid)) {
+      DeleteRequestBuilder deleteResponse = ElasticsearchConfiguration.client.prepareDelete(indexName+"_"+tableTypeName.toLowerCase(), tableTypeName, eid);
+      deleteResponse.execute().actionGet();
+    }
 
-   }
+  }
 
   /**
    * 修改
@@ -238,8 +236,9 @@ public class ElasticSearchService {
     if(CommonUtil.isEmpty(eid)) {
       saveESJson(jsonStr, tableTypeName);
     }else{
+      Map map =JSONObject.parseObject(jsonStr);
       UpdateRequestBuilder updateRequestBuilder = ElasticsearchConfiguration.client.prepareUpdate(indexName+"_"+tableTypeName.toLowerCase(),tableTypeName,eid);
-      updateRequestBuilder.setDoc(jsonStr).get();
+      updateRequestBuilder.setDoc(map).get();
     }
   }
 
@@ -252,9 +251,9 @@ public class ElasticSearchService {
         Object value = entry.getValue();
         String[] values =  StringUtils.split(String.valueOf(value),",");
         if(values.length>1){
-            subCodeQuery.must(QueryBuilders.termsQuery(key.toString(), Arrays.asList(values)));
+          subCodeQuery.must(QueryBuilders.termsQuery(key.toString(), Arrays.asList(values)));
         }else {
-            subCodeQuery.must(QueryBuilders.termQuery(key.toString(), value));
+          subCodeQuery.must(QueryBuilders.termQuery(key.toString(), value));
         }
       }
     }
