24018f00152959431f9513c65108dc5823b6fc9b
qiyuxiang <qiyuxiangjava@qq.com>
2019-11-28 11:38:03 +0800

    支付流程优化

--- dangjia-subgroup/dangjia-upload/dangjia-service-upload/src/main/java/com/dangjia/acg/SesameServiceUploadApplication.java
+++ dangjia-subgroup/dangjia-upload/dangjia-service-upload/src/main/java/com/dangjia/acg/SesameServiceUploadApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * @author: QiYuXiang
@@ -21,6 +24,12 @@ public class SesameServiceUploadApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SesameServiceUploadApplication.class, args);
+  }
+  @Bean
+  MultipartConfigElement multipartConfigElement() {
+    MultipartConfigFactory factory = new MultipartConfigFactory();
+    factory.setLocation("/data/temporary/");
+    return factory.createMultipartConfig();
   }
   @Bean
   EmbeddedServletContainerCustomizer containerCustomizer() throws Exception {
