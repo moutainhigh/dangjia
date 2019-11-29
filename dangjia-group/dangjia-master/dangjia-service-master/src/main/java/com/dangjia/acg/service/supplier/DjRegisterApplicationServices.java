09ffcbab9427bf1bb15d57e4628e828cb55f253b
wukang <1499913860@qq.com>
2019-11-29 09:13:06 +0800

    订单详情

--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/supplier/DjRegisterApplicationServices.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/supplier/DjRegisterApplicationServices.java
@@ -61,48 +61,48 @@ public class DjRegisterApplicationServices {
     private IJobMapper jobMapper;
 
 
-    public String getUserIdExamine(String userId )
-    {
+    public String getUserIdExamine(String userId) {
         return djRegisterApplicationMapper.getUserIdExamine(userId);
     }
 
     /**
      * 供应商/店铺注册
+     *
      * @param djRegisterApplication
      * @return
      */
     public ServerResponse registerSupAndStorefront(DjRegisterApplication djRegisterApplication) {
         try {
-            if(CommonUtil.isEmpty(djRegisterApplication.getUserName()))
+            if (CommonUtil.isEmpty(djRegisterApplication.getUserName()))
                 return ServerResponse.createByErrorMessage("用户名不能为空");
-            if(CommonUtil.isEmpty(djRegisterApplication.getMobile()))
+            if (CommonUtil.isEmpty(djRegisterApplication.getMobile()))
                 return ServerResponse.createByErrorMessage("电话号码不能为空");
-            if(CommonUtil.isEmpty(djRegisterApplication.getCardNumber()))
+            if (CommonUtil.isEmpty(djRegisterApplication.getCardNumber()))
                 return ServerResponse.createByErrorMessage("身份证号码不能为空");
-            if(CommonUtil.isEmpty(djRegisterApplication.getCityId()))
+            if (CommonUtil.isEmpty(djRegisterApplication.getCityId()))
                 return ServerResponse.createByErrorMessage("城市不能为空");
-            if(CommonUtil.isEmpty(djRegisterApplication.getCardNumber()))
+            if (CommonUtil.isEmpty(djRegisterApplication.getCardNumber()))
                 return ServerResponse.createByErrorMessage("身份证不能为空");
-            if(CommonUtil.isEmpty(djRegisterApplication.getPayPassword()))
+            if (CommonUtil.isEmpty(djRegisterApplication.getPayPassword()))
                 return ServerResponse.createByErrorMessage("支付密码不能为空");
-            if(djRegisterApplication.getPayPassword().length()!=6)
+            if (djRegisterApplication.getPayPassword().length() != 6)
                 return ServerResponse.createByErrorMessage("请设置6位数支付密码");
-            Example example=new Example(DjRegisterApplication.class);
-            example.createCriteria().andEqualTo(DjRegisterApplication.CITY_ID,djRegisterApplication.getCityId())
-                    .andEqualTo(DjRegisterApplication.DATA_STATUS,0)
-                    .andEqualTo(DjRegisterApplication.MOBILE,djRegisterApplication.getMobile())
-                    .andNotEqualTo(DjRegisterApplication.APPLICATION_STATUS,2)
-                    .andEqualTo(DjRegisterApplication.APPLICATION_TYPE,djRegisterApplication.getApplicationType());
-            if(djRegisterApplicationMapper.selectByExample(example).size()>0)
+            Example example = new Example(DjRegisterApplication.class);
+            example.createCriteria().andEqualTo(DjRegisterApplication.CITY_ID, djRegisterApplication.getCityId())
+                    .andEqualTo(DjRegisterApplication.DATA_STATUS, 0)
+                    .andEqualTo(DjRegisterApplication.MOBILE, djRegisterApplication.getMobile())
+                    .andNotEqualTo(DjRegisterApplication.APPLICATION_STATUS, 2)
+                    .andEqualTo(DjRegisterApplication.APPLICATION_TYPE, djRegisterApplication.getApplicationType());
+            if (djRegisterApplicationMapper.selectByExample(example).size() > 0)
                 return ServerResponse.createByErrorMessage("申请已存在");
             djRegisterApplication.setDataStatus(0);
             djRegisterApplication.setApplicationStatus(0);
             djRegisterApplication.setPassWord(DigestUtils.md5Hex(djRegisterApplication.getPassWord()));
             djRegisterApplication.setPayPassword(DigestUtils.md5Hex(djRegisterApplication.getPayPassword()));
-            if(djRegisterApplicationMapper.insert(djRegisterApplication)>0)
+            if (djRegisterApplicationMapper.insert(djRegisterApplication) > 0)
                 return ServerResponse.createBySuccessMessage("申请成功");
         } catch (Exception e) {
-            logger.error("申请失败",e);
+            logger.error("申请失败", e);
             return ServerResponse.createByErrorMessage("申请失败");
         }
         return ServerResponse.createByErrorMessage("申请失败");
@@ -110,38 +110,38 @@ public class DjRegisterApplicationServices {
 
 
     /**
-     *
      * @param mobile
      * @return
      */
-    public ServerResponse querySupAndStorefront(String mobile,String cityId) {
+    public ServerResponse querySupAndStorefront(String mobile, String cityId) {
         try {
-            Example example=new Example(DjRegisterApplication.class);
-            example.createCriteria().andEqualTo(DjRegisterApplication.CITY_ID,cityId)
-                    .andEqualTo(DjRegisterApplication.DATA_STATUS,0)
-                    .andEqualTo(DjRegisterApplication.MOBILE,mobile)
-                    .andNotEqualTo(DjRegisterApplication.APPLICATION_STATUS,2);
+            Example example = new Example(DjRegisterApplication.class);
+            example.createCriteria().andEqualTo(DjRegisterApplication.CITY_ID, cityId)
+                    .andEqualTo(DjRegisterApplication.DATA_STATUS, 0)
+                    .andEqualTo(DjRegisterApplication.MOBILE, mobile)
+                    .andNotEqualTo(DjRegisterApplication.APPLICATION_STATUS, 2);
             List<DjRegisterApplication> djRegisterApplications = djRegisterApplicationMapper.selectByExample(example);
-            if(djRegisterApplications.size()>0)
-                return ServerResponse.createBySuccess("查询成功",djRegisterApplications.get(0));
+            if (djRegisterApplications.size() > 0)
+                return ServerResponse.createBySuccess("查询成功", djRegisterApplications.get(0));
         } catch (Exception e) {
-            logger.error("查询失败",e);
+            logger.error("查询失败", e);
             return ServerResponse.createByErrorMessage("查询失败");
         }
         return ServerResponse.createByErrorMessage("查询失败");
     }
 
 
-     /**
+    /**
      * 提交审核注册的供应商或店铺
-     * @param registerId 申请ID
-     * @param isAdopt 是否通过 1=通过  2=不通过
+     *
+     * @param registerId   申请ID
+     * @param isAdopt      是否通过 1=通过  2=不通过
      * @param departmentId 部门ID
-     * @param jobId  岗位ID
-      * @param failReason 不通过原因
+     * @param jobId        岗位ID
+     * @param failReason   不通过原因
      * @return
      */
-    public ServerResponse checkSupAndStorefront(HttpServletRequest request, String registerId, Integer isAdopt, String departmentId, String jobId,String failReason) {
+    public ServerResponse checkSupAndStorefront(HttpServletRequest request, String registerId, Integer isAdopt, String departmentId, String jobId, String failReason) {
         try {
 
             String userID = request.getParameter(Constants.USERID);
@@ -152,17 +152,17 @@ public class DjRegisterApplicationServices {
             djRegisterApplication.setAuditUserId(userID);//审核人
             djRegisterApplication.setModifyDate(new Date());//最后一次操作时间
             djRegisterApplicationMapper.updateByPrimaryKeySelective(djRegisterApplication);
-            Map map=new HashMap();
-            if("1".equals(djRegisterApplication.getApplicationType())){
-                map.put("name","店铺");
+            Map map = new HashMap();
+            if ("1".equals(djRegisterApplication.getApplicationType())) {
+                map.put("name", "店铺");
             }
-            if("2".equals(djRegisterApplication.getApplicationType())){
-                map.put("name","供应商");
+            if ("2".equals(djRegisterApplication.getApplicationType())) {
+                map.put("name", "供应商");
             }
-            if(isAdopt==1){
-                map.put("checkType","已通过");
+            if (isAdopt == 1) {
+                map.put("checkType", "已通过");
                 //注入用户信息
-                MainUser user=new MainUser();
+                MainUser user = new MainUser();
                 user.setPassword(djRegisterApplication.getPassWord());
                 user.setUsername(djRegisterApplication.getName());
                 user.setMobile(djRegisterApplication.getMobile());
@@ -172,13 +172,13 @@ public class DjRegisterApplicationServices {
                 user.setJobId(jobId);
                 user.setPayPassword(djRegisterApplication.getPayPassword());
                 addUser(user);
-            }else{
-                map.put("checkType","未通过，原因:"+failReason);
+            } else {
+                map.put("checkType", "未通过，原因:" + failReason);
             }
             //发送短信通知至注册申请人手机成功与否
-            JsmsUtil.sendSMS(djRegisterApplication.getMobile(),"171137",map);
+            JsmsUtil.sendSMS(djRegisterApplication.getMobile(), "171137", map);
         } catch (Exception e) {
-            logger.error("操作失败",e);
+            logger.error("操作失败", e);
             return ServerResponse.createByErrorMessage("操作失败");
         }
         return ServerResponse.createBySuccessMessage("操作成功");
@@ -193,7 +193,7 @@ public class DjRegisterApplicationServices {
         if (null != existUser) {
             user.setId(existUser.getId());
             this.userMapper.updateByPrimaryKeySelective(user);
-        }else {
+        } else {
             // 新增用户
             user.setId((int) (Math.random() * 50000000) + 50000000 + "" + System.currentTimeMillis());
             user.setCreateDate(new Date());
@@ -206,18 +206,19 @@ public class DjRegisterApplicationServices {
 
     /**
      * 查询已申请注册的用户列表
+     *
      * @param pageDTO
      * @return
      */
-    public ServerResponse<PageInfo> getRegisterList(PageDTO pageDTO,String applicationStatus,String searchKey){
+    public ServerResponse<PageInfo> getRegisterList(PageDTO pageDTO, String applicationStatus, String searchKey) {
         PageHelper.startPage(pageDTO.getPageNum(), pageDTO.getPageSize());
-        try{
-            List<RegisterApplicationDTO> registerList=djRegisterApplicationMapper.getAllRegistList(applicationStatus,searchKey);
+        try {
+            List<RegisterApplicationDTO> registerList = djRegisterApplicationMapper.getAllRegistList(applicationStatus, searchKey);
             PageInfo pageResult = new PageInfo(registerList);
             pageResult.setList(registerList);
             return ServerResponse.createBySuccess("查询成功", pageResult);
-        }catch (Exception e){
-            logger.error("查询失败",e);
+        } catch (Exception e) {
+            logger.error("查询失败", e);
             return ServerResponse.createByErrorMessage("查询失败");
         }
 
@@ -225,13 +226,14 @@ public class DjRegisterApplicationServices {
 
     /**
      * 根据申请ID查询对应的申请信息
+     *
      * @param id 审核ID
      * @return
      */
-    public ServerResponse getRegisterInfoById(String id){
-        try{
+    public ServerResponse getRegisterInfoById(String id) {
+        try {
             String address = configUtil.getValue(SysConfig.PUBLIC_DANGJIA_ADDRESS, String.class);
-            DjRegisterApplication djRegisterApplication=djRegisterApplicationMapper.selectByPrimaryKey(id);
+            DjRegisterApplication djRegisterApplication = djRegisterApplicationMapper.selectByPrimaryKey(id);
             String[] imgArr = djRegisterApplication.getCardImage().split(",");
             StringBuilder imgStr = new StringBuilder();
             StringBuilder imgUrlStr = new StringBuilder();
@@ -239,22 +241,22 @@ public class DjRegisterApplicationServices {
             djRegisterApplication.setCardImage(imgStr.toString());
             Map<String, Object> map = BeanUtils.beanToMap(djRegisterApplication);
             map.put("cardImageUrl", imgUrlStr.toString());
-            map.put("businessLicenseUrl", address+djRegisterApplication.getBusinessLicense());
-            City city =iCityMapper.selectByPrimaryKey(djRegisterApplication.getCityId());
-            if(city!=null&&StringUtils.isNotBlank(city.getId())){
-                map.put("cityName",city.getName());
+            map.put("businessLicenseUrl", address + djRegisterApplication.getBusinessLicense());
+            City city = iCityMapper.selectByPrimaryKey(djRegisterApplication.getCityId());
+            if (city != null && StringUtils.isNotBlank(city.getId())) {
+                map.put("cityName", city.getName());
             }
             //查询用户当前已拥有的部门、岗位
             MainUser mainUser = userMapper.findUserByMobile(djRegisterApplication.getUserName());
-            if(mainUser!=null&&StringUtils.isNotBlank(mainUser.getJobId())){
+            if (mainUser != null && StringUtils.isNotBlank(mainUser.getJobId())) {
                 Job job = jobMapper.selectByPrimaryKey(mainUser.getJobId());
                 //获取部门名称，岗位名称
-                map.put("departmentName",job.getDepartmentName());
-                map.put("jobName",job.getName());
+                map.put("departmentName", job.getDepartmentName());
+                map.put("jobName", job.getName());
             }
             return ServerResponse.createBySuccess("查询成功", map);
-        }catch (Exception e){
-            logger.error("查询失败",e);
+        } catch (Exception e) {
+            logger.error("查询失败", e);
             return ServerResponse.createByErrorMessage("查询失败");
         }
 
@@ -263,28 +265,28 @@ public class DjRegisterApplicationServices {
 
     /**
      * 申请新身份
+     *
      * @param djRegisterApplication
      * @return
      */
-    public ServerResponse insertApplyNewStatus(String userId,DjRegisterApplication djRegisterApplication) {
+    public ServerResponse insertApplyNewStatus(String userId, DjRegisterApplication djRegisterApplication) {
         try {
             MainUser mainUser = userMapper.selectByPrimaryKey(userId);
-            Example example=new Example(DjRegisterApplication.class);
-            example.createCriteria().andEqualTo(DjRegisterApplication.CITY_ID,djRegisterApplication.getCityId())
-                    .andEqualTo(DjRegisterApplication.DATA_STATUS,0)
-                    .andEqualTo(DjRegisterApplication.MOBILE,mainUser.getMobile())
-                    .andNotEqualTo(DjRegisterApplication.APPLICATION_STATUS,2)
-                    .andLike(DjRegisterApplication.APPLICATION_TYPE,"%"+djRegisterApplication.getApplicationType()+"%");
-            List<DjRegisterApplication> djRegisterApplications = djRegisterApplicationMapper.selectByExample(example);
-            if(djRegisterApplications.size()>0)
+            Map<String,Object> map=new HashMap();
+            map.put("cityId",djRegisterApplication.getCityId());
+            map.put("mobile",mainUser.getMobile());
+            djRegisterApplication.getApplicationType().split(",");
+            map.put("split",djRegisterApplication.getApplicationType().split(","));
+            List<DjRegisterApplication> djRegisterApplications = djRegisterApplicationMapper.queryDeWeight(map);
+            if (djRegisterApplications.size() > 0)
                 return ServerResponse.createByErrorMessage("请勿重复申请");
-            example=new Example(DjRegisterApplication.class);
-            example.createCriteria().andEqualTo(DjRegisterApplication.CITY_ID,djRegisterApplication.getCityId())
-                    .andEqualTo(DjRegisterApplication.DATA_STATUS,0)
-                    .andEqualTo(DjRegisterApplication.MOBILE,mainUser.getMobile())
-                    .andNotEqualTo(DjRegisterApplication.APPLICATION_STATUS,2);
+            Example example = new Example(DjRegisterApplication.class);
+            example.createCriteria().andEqualTo(DjRegisterApplication.CITY_ID, djRegisterApplication.getCityId())
+                    .andEqualTo(DjRegisterApplication.DATA_STATUS, 0)
+                    .andEqualTo(DjRegisterApplication.MOBILE, mainUser.getMobile())
+                    .andNotEqualTo(DjRegisterApplication.APPLICATION_STATUS, 2);
             djRegisterApplications = djRegisterApplicationMapper.selectByExample(example);
-            if(djRegisterApplications.size()<=0)
+            if (djRegisterApplications.size() <= 0)
                 return ServerResponse.createByErrorMessage("暂无店铺供应商信息,请先注册");
             djRegisterApplication.setUserName(djRegisterApplications.get(0).getUserName());
             djRegisterApplication.setPassWord(djRegisterApplications.get(0).getPassWord());
@@ -292,15 +294,14 @@ public class DjRegisterApplicationServices {
             djRegisterApplication.setMobile(djRegisterApplications.get(0).getMobile());
             djRegisterApplication.setCardNumber(djRegisterApplications.get(0).getCardNumber());
             djRegisterApplication.setCardImage(djRegisterApplications.get(0).getCardImage());
-            if(djRegisterApplicationMapper.insert(djRegisterApplication)>0)
+            if (djRegisterApplicationMapper.insert(djRegisterApplication) > 0)
                 return ServerResponse.createBySuccessMessage("申请成功");
         } catch (Exception e) {
-            logger.error("申请失败",e);
+            logger.error("申请失败", e);
             return ServerResponse.createByErrorMessage("申请失败");
         }
         return ServerResponse.createByErrorMessage("申请失败");
     }
-
 
 
 }
