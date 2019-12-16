cfcb62367e83990c4489a2905d52c80cfc077ab6
fangzhenghong <fzhdao@163.com>
2019-12-16 13:43:07 +0800

    增加判断商品名称和货品名称是否重复和判断

--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/pay/PaymentService.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/pay/PaymentService.java
@@ -90,9 +90,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
-import sun.swing.StringUIClientPropertyKey;
 import tk.mybatis.mapper.entity.Example;
-import tk.mybatis.mapper.util.StringUtil;
 
 import java.math.BigDecimal;
 import java.util.*;
