dee5010c09e18c906baeddf3d7daa403c4e82d3a
qiyuxiang <qiyuxiangjava@qq.com>
2019-12-16 09:39:50 +0800

    优化业主退材料漏洞

--- dangjia-subgroup/dangjia-config/dangjia-common/src/main/java/com/dangjia/acg/modle/core/HouseFlow.java
+++ dangjia-subgroup/dangjia-config/dangjia-common/src/main/java/com/dangjia/acg/modle/core/HouseFlow.java
@@ -87,7 +87,7 @@ public class HouseFlow extends BaseEntity {
 	private Integer grabNumber;//grabmumber
 
     @Column(name = "work_type")
-    @Desc(value = "默认0, 抢单状态，1还没有发布，只是默认房产,2等待被抢，3有工匠抢单,4已采纳")
+    @Desc(value = "默认0, 抢单状态，1还没有发布，只是默认房产,5等待业主支付，2已支付待工匠抢单,3有工匠抢单待确认,4已采纳")
     @ApiModelProperty("默认0, 抢单状态，1还没有发布，只是默认房产,5等待业主支付，2已支付待工匠抢单,3有工匠抢单待确认,4已采纳")
 	private Integer workType;  //worktype
 
