4748c17ca4402e97ee1ce45583522d5833575bc2
qiyuxiang <qiyuxiangjava@qq.com>
2019-12-20 09:08:39 +0800

    入参解密

--- dangjia-subgroup/dangjia-config/dangjia-common/src/main/java/com/dangjia/acg/common/aspect/ReturnAspect.java
+++ dangjia-subgroup/dangjia-config/dangjia-common/src/main/java/com/dangjia/acg/common/aspect/ReturnAspect.java
@@ -1,12 +1,28 @@
 package com.dangjia.acg.common.aspect;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.dangjia.acg.common.constants.Constants;
 import com.dangjia.acg.common.exception.BaseException;
 import com.dangjia.acg.common.exception.ServerCode;
+import com.dangjia.acg.common.request.ParameterRequestWrapper;
+import com.dangjia.acg.common.util.BeanUtils;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.security.crypto.codec.Hex;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Map;
+
+import static com.dangjia.acg.common.util.AES.decrypt;
 
 /**
  * 类说明:系统服务组件Aspect切面Bean
@@ -50,6 +66,36 @@ public class ReturnAspect {
 
     }
 
+    public void setRequestParameter(String[] argNames ,Object[] args){
+        try {
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+                    .getRequest();
+            Map<String, String[]> parameterMap = request.getParameterMap();
+            StringBuilder paramStr = new StringBuilder();
+            for (Map.Entry<String, String[]> param : parameterMap.entrySet()) {
+                paramStr.append(param.getKey());
+            }
+            byte[] dec = decrypt(Hex.decode(paramStr.toString()), Constants.DANGJIA_SESSION_KEY.getBytes(), Constants.DANGJIA_IV.getBytes());
+            JSONObject json = JSON.parseObject(new String(dec));
+            for (int i = 0; i < args.length; i++) {
+                if(args[i] instanceof HttpServletRequest){
+                    ParameterRequestWrapper wrapper = new ParameterRequestWrapper(request, json);
+                    args[i]=wrapper;
+                }else if (args[i] instanceof BigDecimal ||args[i] instanceof String || args[i] instanceof Date || args[i] instanceof Boolean || args[i] instanceof Byte || args[i] instanceof Short || args[i] instanceof Integer || args[i] instanceof Long || args[i] instanceof Float
+                        || args[i] instanceof Double || args[i] instanceof Enum) {
+                    if(json.get(argNames[i])!=null){
+                        args[i]=json.get(argNames[i]);
+                    }
+                }else{
+                    args[i]= BeanUtils.mapToBean(args[i].getClass(),json);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new BaseException(ServerCode.ERROR, "系统繁忙，请稍后再试! "+ServerCode.SERVER_UNKNOWN_ERROR.getCode());
+        }
+    }
     // 配置后置通知,使用在方法aspect()上注册的切入点
     @After("aspect()")
     public void after(JoinPoint joinPoint) {
@@ -63,11 +109,14 @@ public class ReturnAspect {
     @Around("aspect()")
     public Object around(ProceedingJoinPoint joinPoint) {
         String name = joinPoint.getSignature().getName();// 获得目标方法名
+        Object[] args = joinPoint.getArgs();
+        String[] argNames = ((MethodSignature)joinPoint.getSignature()).getParameterNames(); // 参数名
+        setRequestParameter(argNames,args);
         log.info("<=============" + name + "方法--AOP 环绕通知=============>");
         long start = System.currentTimeMillis();
         Object result = null;
         try {
-            result = joinPoint.proceed();
+            result = joinPoint.proceed(args);
             long end = System.currentTimeMillis();
             if (log.isInfoEnabled()) {
                 log.info("around " + joinPoint + "\tUse time : "
