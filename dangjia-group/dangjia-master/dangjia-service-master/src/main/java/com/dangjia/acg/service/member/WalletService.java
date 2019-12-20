67ec625d902440a634dc3a85a5a28338031d2078
qiyuxiang <qiyuxiangjava@qq.com>
2019-12-20 19:19:35 +0800

    资产列表

--- dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/member/WalletService.java
+++ dangjia-group/dangjia-master/dangjia-service-master/src/main/java/com/dangjia/acg/service/member/WalletService.java
@@ -468,6 +468,7 @@ public class WalletService {
         Example example = new Example(WorkerDetail.class);
         Example.Criteria criteria= example.createCriteria();
         criteria.andLessThanOrEqualTo(WorkerDetail.CREATE_DATE,time);
+        criteria.andGreaterThanOrEqualTo(WorkerDetail.CREATE_DATE,DateUtil.getDateString(DateUtil.getMonthFirst(DateUtil.toDate(time),-12).getTime()));
         criteria.andEqualTo(WorkerDetail.WORKER_ID,member.getId());
         if(state!=null){
             criteria.andIn(WorkerDetail.STATE,Arrays.asList(state));
