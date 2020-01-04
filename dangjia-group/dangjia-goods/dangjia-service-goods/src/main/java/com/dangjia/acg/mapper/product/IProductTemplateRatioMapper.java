73f39c20268b28c58f26f6c9969f9158caea6e72
fangzhenghong <fzhdao@163.com>
2020-01-04 11:39:03 +0800

    增加维保商品的添、删、改、查

---
+++ dangjia-group/dangjia-goods/dangjia-service-goods/src/main/java/com/dangjia/acg/mapper/product/IProductTemplateRatioMapper.java
@@ -0,0 +1,12 @@
+package com.dangjia.acg.mapper.product;
+
+import com.dangjia.acg.modle.product.BasicsProductTemplateRatio;
+import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
+
+
+@Repository
+public interface IProductTemplateRatioMapper extends Mapper<BasicsProductTemplateRatio> {
+
+}
+
