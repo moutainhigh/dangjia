5122948dda8508a5525b8f94a16597b00a994361
fangzhenghong <fzhdao@163.com>
2020-01-07 15:46:21 +0800

    维保商品，及责任占比划分表优化。

--- dangjia-subgroup/dangjia-config/dangjia-common/src/main/java/com/dangjia/acg/modle/engineer/DjMaintenanceRecordProduct.java
+++ dangjia-subgroup/dangjia-config/dangjia-common/src/main/java/com/dangjia/acg/modle/engineer/DjMaintenanceRecordProduct.java
@@ -44,4 +44,40 @@ public class DjMaintenanceRecordProduct extends BaseEntity {
     @ApiModelProperty("单价")
     private Double price;
 
+    @Column(name = "worker_type_id")
+    @Desc(value = "工种ID")
+    @ApiModelProperty("工种ID")
+    private String workerTypeId;
+
+    @Column(name = "maintenance_member_id")
+    @Desc(value = "维护人ID")
+    @ApiModelProperty("维护人ID")
+    private String maintenanceMemberId;
+
+    @Column(name = "maintenance_member_type")
+    @Desc(value = "维护人类型：1业主；2大管家；3工匠")
+    @ApiModelProperty("维护人类型：1业主；2大管家；3工匠")
+    private Integer  maintenanceMemberType;
+
+    @Column(name = "total_price")
+    @Desc(value = "商品总价")
+    @ApiModelProperty("商品总价")
+    private Double totalPrice;
+
+    @Column(name = "pay_price")
+    @Desc(value = "需支付价钱")
+    @ApiModelProperty("需支付价钱")
+    private Double payPrice;
+
+    @Column(name = "over_protection")
+    @Desc(value = "是否过保商品（1是，0否）")
+    @ApiModelProperty("是否过保商品（1是，0否）")
+    private Integer over_protection;
+
+    @Column(name = "pay_state")
+    @Desc(value = "是否已付费（1未支付，2已支付）")
+    @ApiModelProperty("是否已付费（1未支付，2已支付）")
+    private Integer payState;
+
+
 }
