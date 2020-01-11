c3efd1831c5efbce950f75af1548ac3eb2550967
qiyuxiang <qiyuxiangjava@qq.com>
2020-01-11 17:54:15 +0800

    大管家巡查 第一步+第二步接口优化

--- dangjia-group/dangjia-master/dangjia-api-master/src/main/java/com/dangjia/acg/dto/matter/TechnologyRecordDTO.java
+++ dangjia-group/dangjia-master/dangjia-api-master/src/main/java/com/dangjia/acg/dto/matter/TechnologyRecordDTO.java
@@ -2,6 +2,8 @@ package com.dangjia.acg.dto.matter;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+
 /**
  * author: Ronalcheng
  * Date: 2018/12/26 0026
@@ -9,7 +11,15 @@ import lombok.Data;
  */
 @Data
 public class TechnologyRecordDTO {
-    private String id;
-    private String name;
+    private String id;//节点ID
+    private String name;//节点名称
     private Integer state;//验收状态0:未验收;1:已验收,2:已退, 3:勾选中
+
+
+
+    @Column(name = "worker_type_id")
+    private String workerTypeId;//工种id 如果是人工工艺有工种id
+
+    @Column(name = "image")
+    private String image;//图片
 }
